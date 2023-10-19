55ff30cd20842b136fa14d6eeebaffac0e19cc77
이승민 <dltmdals1620@gmail.com>
2023-10-19 16:07:27 +0900

    Feat: email find 클래스 구현

---
+++ back/letter_spring/letter/src/main/java/com/letter/letter/member/respository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.letter.letter.member.respository;
+
+import com.letter.letter.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+}
