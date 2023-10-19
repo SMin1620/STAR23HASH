eeb7165c71b2edc4dadada9f9859deea6467ebef
이승민 <dltmdals1620@gmail.com>
2023-10-19 16:05:20 +0900

    Feat: 멤버 엔티티 설계

---
+++ back/letter_spring/letter/src/main/java/com/letter/letter/member/entity/Member.java
@@ -0,0 +1,35 @@
+package com.letter.letter.member.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "members")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    private Role role;
+
+    @Column(nullable = false)
+    private LocalDateTime createAt;
+
+}
