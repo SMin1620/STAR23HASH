c14bfeba9bb2233379adb4ca6fe9cfff626b63bd
이승민 <dltmdals1620@gmail.com>
2023-10-19 15:45:35 +0900

    Feat: 메인 어노테이션 추가

--- back/letter_spring/letter/src/main/java/com/letter/letter/LetterApplication.java
+++ back/letter_spring/letter/src/main/java/com/letter/letter/LetterApplication.java
@@ -2,7 +2,11 @@ package com.letter.letter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableJpaAuditing
+@EnableAsync
 @SpringBootApplication
 public class LetterApplication {
 
