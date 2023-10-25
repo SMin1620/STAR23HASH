4c78a17bb4ab345b9eb3603ea18935d56d8016dc
이승민 <dltmdals1620@gmail.com>
2023-10-25 14:31:57 +0900

    Feat : 익명의 편지 서버 구축
    Change-Id: Ifde29fc28c15f7c5b1c6df0884794f005fbcbb27

---
+++ back/letter_spring/letter-service/src/main/java/com/letter/letterservice/LetterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.letter.letterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetterServiceApplication.class, args);
+	}
+
+}
