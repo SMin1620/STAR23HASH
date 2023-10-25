2486c52aa0b48d4f26e7b631067f4943defaeb50
이승민 <dltmdals1620@gmail.com>
2023-10-25 15:21:04 +0900

    Fix : 인증 서버 - 유저 컨트롤러 불필요한 부분 삭제 및 이메일을 번호로 수정
    Change-Id: I501ec635e1c5c3724d2f48b717fd56c250d19423

--- back/letter_spring/auth-service/src/main/java/com/letter/authservice/member/controller/MemberController.java
+++ back/letter_spring/auth-service/src/main/java/com/letter/authservice/member/controller/MemberController.java
@@ -46,30 +46,6 @@ public class MemberController {
         return new BaseResponse(HttpStatus.OK, "회원가입 성공", memberMapper.memberToResponseDto(member));
     }
 
-    /**
-     * 이메일 중복 확인
-     */
-    @GetMapping("/email")
-    public BaseResponse emailDoubleCheck(@RequestParam String email){
-        boolean isDouble = memberService.emailDoubleCheck(email);
-
-        if(isDouble){
-            return new BaseResponse(HttpStatus.OK, "이메일 중복 체크 성공", true);
-        }else{
-            return new BaseResponse(HttpStatus.OK, "이메일 중복 체크 성공", false);
-        }
-    }
-
-    /**
-     * 닉네임 중복 확인(api 명세서랑 다름)
-     */
-    @GetMapping("/nickname")
-    public BaseResponse nicknameDoubleCheck(@RequestParam String nickname){
-        boolean isDouble = memberService.nicknameDoubleCheck(nickname);
-
-        if(isDouble) return new BaseResponse(HttpStatus.OK, "닉네임 중복 체크 성공", true);
-        else return new BaseResponse(HttpStatus.OK, "닉네임 중복 체크 성공", false);
-    }
 
     /**
      * 멤버 정보 확인(api 명세서랑 다름)
@@ -108,23 +84,6 @@ public class MemberController {
             e.printStackTrace();
             throw new BusinessLogicException(ExceptionCode.INVALID_REFRESH_TOKEN);
         }
-    }
-
-        /**
-     * 이미지 리롤
-     */
-    @GetMapping("/reroll")
-    public BaseResponse reroll(
-            HttpServletRequest request
-            ) {
-        String token = jwtTokenProvider.resolveToken(request);
-        jwtTokenProvider.validateToken(token);
-
-        String memberEmail = jwtTokenProvider.getUserEmail(token);
-
-        Member member = memberService.reroll(memberEmail);
-
-        return new BaseResponse(HttpStatus.OK, "reroll 성공", memberMapper.toResponse(member));
     }
 
 }
