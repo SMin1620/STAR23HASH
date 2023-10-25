c5a075eb821ff253dd0f7372f37ea8960a2ea99c
이승민 <dltmdals1620@gmail.com>
2023-10-25 12:00:37 +0900

    Fix : 게이트웨이 서버 - 웹 플럭스 시큐리티 csrf 임시 해제
    Change-Id: Ifa119a0d3737813c138d8dd94599d4f96e52f02a

--- back/letter_spring/gateway/src/main/java/com/letter/gateway/GatewayApplication.java
+++ back/letter_spring/gateway/src/main/java/com/letter/gateway/GatewayApplication.java
@@ -2,6 +2,9 @@ package com.letter.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -10,4 +13,8 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	@Bean
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity security) {
+		return security.csrf().disable().build();
+	}
 }
