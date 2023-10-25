5d679cb58a50750205a003cb136beebfe4a39eb7
이승민 <dltmdals1620@gmail.com>
2023-10-25 12:43:27 +0900

    Feat : 인증 서버 - 환경 변수 빈 설정
    Change-Id: I0afb225dff5623e7b43916fe583660a522ced7b8

---
+++ back/letter_spring/auth-service/src/main/java/com/letter/authservice/auth/PropertyConfig.java
@@ -0,0 +1,12 @@
+package com.letter.authservice.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+        @PropertySource("classpath:properties/env.properties") // env.properties 파일 소스 등록
+})
+public class PropertyConfig {
+}
