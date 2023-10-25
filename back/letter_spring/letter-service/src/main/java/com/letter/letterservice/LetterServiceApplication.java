b517e5545423095f08fb6107d394027db520f833
이승민 <dltmdals1620@gmail.com>
2023-10-25 14:47:13 +0900

    Feat : 익명의 편지 서비스 - 유레카 서버에 마이크로 서비스 등록
    Change-Id: I07c6e0e86974e8e7be453878a7395196d7f15e4b

--- back/letter_spring/letter-service/src/main/java/com/letter/letterservice/LetterServiceApplication.java
+++ back/letter_spring/letter-service/src/main/java/com/letter/letterservice/LetterServiceApplication.java
@@ -2,8 +2,10 @@ package com.letter.letterservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LetterServiceApplication {
 
 	public static void main(String[] args) {
