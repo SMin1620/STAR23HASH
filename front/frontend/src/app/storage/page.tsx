01fcd2a32c6fc217cca86b614a93c08ca0184229
조병철 <zxas1245@naver.com>
2023-10-26 17:30:49 +0900

    Test: SVG file upload test
    Change-Id: I995e8964a936683f18ae25c2394bf47dc9720b9f

--- front/frontend/src/app/storage/page.tsx
+++ front/frontend/src/app/storage/page.tsx
@@ -1,20 +1,32 @@
 import Link from "next/link";
-import GlobalStyle from "./GlobalStyles";
+import GlobalStyle from "./GlobalStyles"
+
+import FriendBtn from '@/component/icons/FriendStorageBtn.svg'
+import RandonBtn from '@/component/icons/RandomStorageBtn.svg'
+import * as st from "./storage.styled"
 
 export default function Storage() {
   return (
     <>
       <GlobalStyle />
       <div className="Title">요기는 스토리징~</div>
-      <div>
+      <st.TestText>
         <Link href={"/storage/random"}>랜덤보관함으로 가깅</Link>
-      </div>
-      <div>
+      </st.TestText>
+      <st.TestText>
         <Link href={"/storage/friend"}>칭구보관함으로 가깅</Link>
-      </div>
-      <div>
+      </st.TestText>
+      <st.TestText>
         <Link href={"/"}>호무</Link>
-      </div>
+      </st.TestText>
+      <st.NavigationBox>
+        <st.FriendTitle>친구</st.FriendTitle>
+      <FriendBtn width={"100%"} height={"100%"}/>
+      </st.NavigationBox>
+      <st.NavigationBox>
+        <st.RandomTitle>랜덤</st.RandomTitle>
+      <RandonBtn width={"100%"} height={"100%"}/>
+      </st.NavigationBox>
     </>
   );
 }
