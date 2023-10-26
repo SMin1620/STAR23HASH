5125f243b5c2fff695fea61fe602951b8ac0b874
조병철 <zxas1245@naver.com>
2023-10-26 17:47:35 +0900

    Refactor: Apply Preetier
    Change-Id: I2bc16ce3189bb38a0bd50a025d647542427b93b9

--- front/frontend/src/app/storage/friend/inbox/page.tsx
+++ front/frontend/src/app/storage/friend/inbox/page.tsx
@@ -1,18 +1,18 @@
-import Link from "next/link";
+import Link from 'next/link'
 
-export default function Inbox(){
-    return  <>
-    <div className="Title">
-        요기는 인박스~
-    </div>
-    <div>
-<Link href={'/storage/random'}>랜덤보관함으로 가깅</Link>
-</div>
-<div>
-<Link href={'/storage/friend'}>칭구보관함으로 가깅</Link>
-</div>
-<div>
-<Link href={'/'}>호무</Link>
-</div>
-</>
+export default function Inbox() {
+  return (
+    <>
+      <div className="Title">요기는 인박스~</div>
+      <div>
+        <Link href={'/storage/random'}>랜덤보관함으로 가깅</Link>
+      </div>
+      <div>
+        <Link href={'/storage/friend'}>칭구보관함으로 가깅</Link>
+      </div>
+      <div>
+        <Link href={'/'}>호무</Link>
+      </div>
+    </>
+  )
 }
--- front/frontend/src/app/storage/random/reply/page.tsx
+++ front/frontend/src/app/storage/random/reply/page.tsx
@@ -1,18 +1,18 @@
-import Link from "next/link";
+import Link from 'next/link'
 
-export default function Letter(){
-    return  <>
-    <div className="Title">
-        요기는 랜덤친구 답장~
-    </div>
-    <div>
-<Link href={'/storage/random'}>랜덤보관함으로 가깅</Link>
-</div>
-<div>
-<Link href={'/storage/friend'}>칭구보관함으로 가깅</Link>
-</div>
-<div>
-<Link href={'/'}>호무</Link>
-</div>
-</>
+export default function Letter() {
+  return (
+    <>
+      <div className="Title">요기는 랜덤친구 답장~</div>
+      <div>
+        <Link href={'/storage/random'}>랜덤보관함으로 가깅</Link>
+      </div>
+      <div>
+        <Link href={'/storage/friend'}>칭구보관함으로 가깅</Link>
+      </div>
+      <div>
+        <Link href={'/'}>호무</Link>
+      </div>
+    </>
+  )
 }
