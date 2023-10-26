7191c1e6596d412127bcba006ec95af5c7ce8187
조병철 <zxas1245@naver.com>
2023-10-26 14:49:24 +0900

    Test: Styled-component apply test
    styled-component 를 적용하는 방식에 대해 테스트 중입니다.
    
    Change-Id: I58e93fc7be404a28d01d1a365ad5fd84198ef3af

---
+++ front/frontend/src/app/storage/GlobalStyles.ts
@@ -0,0 +1,29 @@
+"use client";
+
+import { createGlobalStyle, css } from "styled-components";
+
+import reset from "styled-reset";
+
+const FlexCenter = css`
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+*{
+    box-sizing:border-box;
+}
+body{
+    background-color: #ffd4f7;
+
+    margin-top: 5rem;
+    ${FlexCenter}
+
+}
+
+`;
+
+export default GlobalStyle;
--- front/frontend/src/app/storage/page.tsx
+++ front/frontend/src/app/storage/page.tsx
@@ -1,23 +1,20 @@
 import Link from "next/link";
+import GlobalStyle from "./GlobalStyles";
 
-export default function Storage(){
-
-
-    return (
-        <>
-            <div className="Title">
-                요기는 스토리징~
-            </div>
-            <div>
-    <Link href={'/storage/random'}>랜덤보관함으로 가깅</Link>
-    </div>
-    <div>
-    <Link href={'/storage/friend'}>칭구보관함으로 가깅</Link>
-    </div>
-    <div>
-<Link href={'/'}>호무</Link>
-</div>
+export default function Storage() {
+  return (
+    <>
+      <GlobalStyle />
+      <div className="Title">요기는 스토리징~</div>
+      <div>
+        <Link href={"/storage/random"}>랜덤보관함으로 가깅</Link>
+      </div>
+      <div>
+        <Link href={"/storage/friend"}>칭구보관함으로 가깅</Link>
+      </div>
+      <div>
+        <Link href={"/"}>호무</Link>
+      </div>
     </>
-    );
-
+  );
 }
