1f6b09451f560a278f63671497237ed2e9bc0dfb
조병철 <zxas1245@naver.com>
2023-10-26 17:30:49 +0900

    Test: SVG file upload test
    Change-Id: I995e8964a936683f18ae25c2394bf47dc9720b9f

--- front/frontend/src/app/storage/GlobalStyles.ts
+++ front/frontend/src/app/storage/GlobalStyles.ts
@@ -1,29 +1,28 @@
-"use client";
+"use client"
 
-import { createGlobalStyle, css } from "styled-components";
+import { createGlobalStyle, css } from "styled-components"
 
-import reset from "styled-reset";
+import reset from "styled-reset"
 
 const FlexCenter = css`
   justify-content: center;
   display: flex;
   flex-direction: column;
   align-items: center;
-`;
+`
 
 const GlobalStyle = createGlobalStyle`
 ${reset}
 *{
-    box-sizing:border-box;
+    box-sizing:border-box
 }
 body{
     background-color: #ffd4f7;
 
     margin-top: 5rem;
-    ${FlexCenter}
-
+    ${FlexCenter};
 }
 
-`;
+`
 
-export default GlobalStyle;
+export default GlobalStyle
