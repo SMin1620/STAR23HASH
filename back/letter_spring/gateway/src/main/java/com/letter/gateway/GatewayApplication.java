55366d59677d10e50c021011470ab13a67e0b9aa
이승민 <dltmdals1620@gmail.com>
2023-10-27 14:34:06 +0900

    Fix : 게이트웨이 서버 - 메인 클래스 수정
    Change-Id: Id54e8a4f143aaf2471b69f632925b841f46f4d2c

--- back/letter_spring/gateway/src/main/java/com/letter/gateway/GatewayApplication.java
+++ back/letter_spring/gateway/src/main/java/com/letter/gateway/GatewayApplication.java
@@ -3,8 +3,10 @@ package com.letter.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -13,8 +15,17 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
-	@Bean
-	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity security) {
-		return security.csrf().disable().build();
-	}
+//	@Bean
+//	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity security) {
+//		return security
+//				.authorizeExchange()
+//				.pathMatchers("/swagger-ui/**").permitAll()
+//				.pathMatchers("/webjars/**").permitAll()
+//				.pathMatchers("/swagger-ui.index.html/**").permitAll()
+//				.pathMatchers("/swagger-ui.html").permitAll()
+//				.anyExchange().authenticated()
+//				.and()
+//				.csrf().disable()
+//				.build();
+//	}
 }
