b8d763147bfa25e17630da8d21c14e827df905d7
조병철 <zxas1245@naver.com>
2023-10-27 12:07:06 +0900

    Chore: Add temp storage main logic
    styled-component 로 threejs 적용 전 임시 페이지 , 그런데 tw 적용
    안돼있어서 tw 적용한 css로 다시 작성해야함
    
    Change-Id: I60fbcd6c51cc6a230ee4322bd1f21e7a805a837f

--- front/frontend/src/app/storage/GlobalStyles.ts
+++ front/frontend/src/app/storage/GlobalStyles.ts
@@ -5,7 +5,7 @@ import { createGlobalStyle, css } from 'styled-components'
 import reset from 'styled-reset'
 
 const FlexCenter = css`
-  justify-content: center;
+  // justify-content: center;
   display: flex;
   flex-direction: column;
 `
@@ -13,13 +13,18 @@ const FlexCenter = css`
 const GlobalStyle = createGlobalStyle`
 ${reset}
 
+@font-face {
+  font-family: 'Dovemayo_gothic';
+  src: local('Dovemayo_gothic'), local('Dovemayo_gothic');
+  font-style: normal;
+  src: url(./src/fonts/Dovemayo_gothic.ttf) format('truetype');
+}
+
 *{
     box-sizing:border-box
 }
 body{
   background-image: url('/icons/TempBG_3.svg');
-
-    margin-top: 5rem;
     ${FlexCenter};
 }
 
--- front/frontend/src/app/storage/page.tsx
+++ front/frontend/src/app/storage/page.tsx
@@ -7,17 +7,7 @@ export default function Storage() {
   return (
     <>
       <GlobalStyle />
-      <div className="Title">요기는 스토리징~</div>
-      <st.TestText>
-        <Link href={'/storage/random'}>랜덤보관함으로 가깅</Link>
-      </st.TestText>
-      <st.TestText>
-        <Link href={'/storage/friend'}>칭구보관함으로 가깅</Link>
-      </st.TestText>
-
-      <st.TestText>
-        <Link href={'/'}>호무</Link>
-      </st.TestText>
+      <st.Clock>14:23</st.Clock>
 
       <st.FriendNavBox>
         <st.FriendTitle>친구</st.FriendTitle>
@@ -26,6 +16,16 @@ export default function Storage() {
       <st.RandomNavBox>
         <st.RandomTitle>랜덤</st.RandomTitle>
       </st.RandomNavBox>
+
+      <st.TestText>
+        <Link href={'/storage/random'}>랜덤보관함으로 가깅</Link>
+      </st.TestText>
+      <st.TestText>
+        <Link href={'/storage/friend'}>칭구보관함으로 가깅</Link>
+      </st.TestText>
+      <st.TestText>
+        <Link href={'/'}>호무</Link>
+      </st.TestText>
     </>
   )
 }
