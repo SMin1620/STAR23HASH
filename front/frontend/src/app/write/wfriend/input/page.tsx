67a29697dfcf6ace2fe9c04b1529d16cf30a0215
조은정 <ejjeongs@naver.com>
2023-10-28 21:05:57 +0900

    Feat: 편지보내기 페이지 추가
    편지보내기 UI 구성 진행 중
    
    Change-Id: Ie5f9ca7788a24bf2ba60534d014970c1e7fe2549

--- front/frontend/src/app/write/wfriend/input/page.tsx
+++ front/frontend/src/app/write/wfriend/input/page.tsx
@@ -7,23 +7,35 @@ export default function WriteFriend() {
   return (
     <>
       <GlobalStyle />
-      <stt.SendBox>
-        <stt.InnerCircle>
-          <stt.InnerCircle1></stt.InnerCircle1>
-          <stt.InnerCircle2></stt.InnerCircle2>
-          <stt.InnerCircle3></stt.InnerCircle3>
-          <st.SpaceImg
-            src="/write/send_friend.png"
-            alt="이미지 불러오기 실패"
-          ></st.SpaceImg>
-        </stt.InnerCircle>
-        <st.ContentBox>
-          <st.InputContent></st.InputContent>
-
-          <st.AddContent></st.AddContent>
-        </st.ContentBox>
-        <stt.button>확인</stt.button>
-      </stt.SendBox>
+      <div className="flex h-screen w-screen items-center justify-center">
+        <stt.SendBox className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:w-full">
+          <st.SpaceImgContainer>
+            <stt.InnerCircle>
+              <stt.InnerCircle1></stt.InnerCircle1>
+              <stt.InnerCircle2></stt.InnerCircle2>
+              <stt.InnerCircle3></stt.InnerCircle3>
+            </stt.InnerCircle>
+            <st.SpaceImg src="/write/Solar System.svg" alt="Solar System" />
+          </st.SpaceImgContainer>
+          <st.ContentBox>
+            <st.InputContent></st.InputContent>
+            <st.Hint>
+              <st.HintCheck type="checkbox" />
+              <st.HintCheckCustom />
+              나에 대한 힌트 주기
+            </st.Hint>
+            <st.AddContent>
+              <st.ContentInfoText>어떤걸 추가 해볼까?</st.ContentInfoText>
+              <st.Medias>
+                <st.Media src="/write/voice.svg" alt="voice"></st.Media>
+                <st.Media src="/write/video.svg" alt="video"></st.Media>
+                <st.Media src="/write/picture.svg" alt="picture"></st.Media>
+              </st.Medias>
+            </st.AddContent>
+            <stt.button>확인</stt.button>
+          </st.ContentBox>
+        </stt.SendBox>
+      </div>
     </>
   )
 }
