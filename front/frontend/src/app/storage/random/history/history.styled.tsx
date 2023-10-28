eb593a52feb1e7e83893d75b9a9dfdc8edc73f21
조병철 <zxas1245@naver.com>
2023-10-28 20:20:52 +0900

    Feat: Modify letter log template
    랜덤 이용자 메시지 로그 UI 구성
    
    Change-Id: Id3eb442bddccc98cba2a68fd70335783fd326acc

--- front/frontend/src/app/storage/random/history/history.styled.tsx
+++ front/frontend/src/app/storage/random/history/history.styled.tsx
@@ -16,6 +16,8 @@ export const RandomMainTitle = styled.div`
   align-items: center;
 `
 export const LetterLogWrapper = styled.div`
+  box-shadow: inset 0px 4px 1px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 1);
   overflow-y: scroll;
   scrollbar-width: none;
   -ms-overflow-style: none;
@@ -23,4 +25,34 @@ export const LetterLogWrapper = styled.div`
   &::-webkit-scrollbar {
     display: none;
   }
+`
+
+export const LetterName = styled.h1`
+  color: #111827;
+  font-size: 1rem;
+  font-weight: bold;
+`
+export const LetterDate = styled.p`
+  color: #696969;
+  font-size: 0.7rem;
+`
+export const LetterContent = styled.span`
+  margin-top: 0.5rem;
+`
+export const LetterReceived = styled.li`
+  background-image: url('/icons/RandomLetterBG.svg');
+`
+export const LetterSent = styled.li`
+position: relative;
+background-image: url('/icons/RandomLetterBG.svg'); 
+&::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(13, 124, 255, 0.2); 
+  border-radius: 10px; 
+  
 `
