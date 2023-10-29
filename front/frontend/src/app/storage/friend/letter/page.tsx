3d5461f728dbdcc74ce8516ffd0f06585c62ea7b
조병철 <zxas1245@naver.com>
2023-10-29 17:12:16 +0900

    Feat: Finish letter draft
    받은 편지 페이지 초안 완료
    
    Change-Id: I3c9f9c83ae1dc0a36b558c630ca40d547e2c33e8

--- front/frontend/src/app/storage/friend/letter/page.tsx
+++ front/frontend/src/app/storage/friend/letter/page.tsx
@@ -4,15 +4,33 @@ export default function Letter() {
   return (
     <>
       <l.LetterBoard className="grid h-screen place-items-center bg-[length:700px] bg-[left_top_15rem]">
-        <l.LetterContainer className="w-80 md:w-2/3 lg:w-2/3">
-          <l.DecoBottonWrapper>
+        <l.LetterContainer className="grid  w-80 md:w-2/3 lg:w-2/3">
+          <l.DecoBottonWrapper className="mb-4 self-start justify-self-start">
             <l.RedDecoBotton />
             <l.YellowDecoBotton />
             <l.GreenDecoBotton />
           </l.DecoBottonWrapper>
+          {/* other type image...sound..video.. */}
+          <l.MediaWrapper>
+            {/* image */}
+            <l.CustonImage
+              className="aspect-auto"
+              src="/icons/planets/Planet-1.svg"
+            />
+            {/* video */}
+            {/* sound */}
+          </l.MediaWrapper>
           {/* content input */}
-
-          <l.CloseBotton>닫기</l.CloseBotton>
+          <l.LetterContentWrapper className=" flex h-fit flex-col rounded-lg p-4">
+            <l.LetterContent>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
+              impedit perferendis suscipit eaque, iste dolor cupiditate
+              blanditiis ratione.
+            </l.LetterContent>
+          </l.LetterContentWrapper>
+          <l.CloseBotton className="mt-4 self-end justify-self-end">
+            닫기
+          </l.CloseBotton>
         </l.LetterContainer>
       </l.LetterBoard>
     </>
