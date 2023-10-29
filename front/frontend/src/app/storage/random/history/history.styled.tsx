f49f470adbebc40f760921e5140e72f887a67b3f
조병철 <zxas1245@naver.com>
2023-10-29 15:08:08 +0900

    Feat: Design friend inbox
    친구 편지함 작성중입니다.
    
    Change-Id: I091cca637e909a893341bcc1cb9fa84827cf7f9a

--- front/frontend/src/app/storage/random/history/history.styled.tsx
+++ front/frontend/src/app/storage/random/history/history.styled.tsx
@@ -8,7 +8,7 @@ export const TestDiv = styled.div`
 
 export const BackBtnWrapper = styled.div`
   margin-top: 0.5rem;
-  margin-right: 0.5rem;
+  margin-left: 0.5rem;
 `
 
 export const CustonImage = styled.img``
