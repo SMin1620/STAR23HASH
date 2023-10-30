c8fe73a5b29a646c1d8995b50afeea5ed745b7ec
un2642 <un2642@naver.com>
2023-10-30 17:08:49 +0900

    Feat: 메인 페이지 Three.js 적용 및 로그인 페이지 제작
    Change-Id: I7b56473f5aa926db6d6b4d6542bac6529b5b1da3

---
+++ front/frontend/src/app/today/delivery/page.tsx
@@ -0,0 +1,34 @@
+import DeliveryThree from '../../../component/Three/delivery'
+
+const Delivery: React.FC = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center">
+        <div className="absolute z-10">
+          <div
+            style={{ marginTop: '380px' }}
+            className="mt-20 flex items-center justify-center pt-20 text-2xl text-white"
+          >
+            편지 배달하는 중...
+          </div>
+        </div>
+        <div className="absolute z-10">
+          <div
+            style={{ marginTop: '900px' }}
+            className="mt-20 flex items-center justify-center pt-20 text-2xl text-white"
+          >
+            남은 시간
+          </div>
+          <div className="flex items-center justify-center text-2xl text-white">
+            18 : 00
+          </div>
+        </div>
+      </div>
+      <DeliveryThree
+        style={{ position: 'absolute', width: '100%', height: '100%' }}
+      />
+    </div>
+  )
+}
+
+export default Delivery
