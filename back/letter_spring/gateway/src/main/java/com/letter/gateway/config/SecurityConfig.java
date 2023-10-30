cbfcd2edba787b05f4b647e2769270a60f659f10
이승민 <dltmdals1620@gmail.com>
2023-10-30 16:30:20 +0900

    Fix : 게이트웨이 서버 - 시큐리티 수정
    Change-Id: I4aece31f2bdd0bf54e0d442d96b088c70b432a85

--- back/letter_spring/gateway/src/main/java/com/letter/gateway/config/SecurityConfig.java
+++ back/letter_spring/gateway/src/main/java/com/letter/gateway/config/SecurityConfig.java
@@ -1,41 +1,44 @@
-package com.letter.gateway.config;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
-
-@Configuration
-@EnableWebFluxSecurity
-@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
-@RequiredArgsConstructor
-public class SecurityConfig {
-
-    @Bean
-    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
-        return http.cors()
-                .and()
-                .csrf()
-                .disable()
-                .httpBasic()
-                .disable()
-                .exceptionHandling()
-                .and()
-                .authorizeExchange()
-                .pathMatchers("/swagger-ui/**").permitAll()
-                .pathMatchers("/webjars/**").permitAll()
-                .pathMatchers("/swagger-ui.index.html/**").permitAll()
-                .pathMatchers("/swagger-ui.html").permitAll()
-                .pathMatchers("/v3/**").permitAll()
-                .pathMatchers("/openapi/**").permitAll()
-                .anyExchange()
-                .authenticated()
-                .and()
-                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
-                .build();
-    }
-}
+//package com.letter.gateway.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+//import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
+//
+//@Configuration
+//@EnableWebFluxSecurity
+//@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
+//@RequiredArgsConstructor
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
+//        return http.cors()
+//                .and()
+//                .csrf()
+//                .disable()
+//                .httpBasic()
+//                .disable()
+//                .exceptionHandling()
+//                .and()
+//                .authorizeExchange()
+//                .pathMatchers("/swagger-ui/**").permitAll()
+//                .pathMatchers("/webjars/**").permitAll()
+//                .pathMatchers("/swagger-ui.index.html/**").permitAll()
+//                .pathMatchers("/swagger-ui.html").permitAll()
+//                .pathMatchers("/v3/**").permitAll()
+//                .pathMatchers("/openapi/**").permitAll()
+//                .pathMatchers("/api/members/register").permitAll()
+//                .pathMatchers("/api/members/login").permitAll()
+//                .pathMatchers("/api/notes/**").authenticated()
+//                .anyExchange()
+//                .authenticated()
+//                .and()
+//                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
+//                .build();
+//    }
+//}
