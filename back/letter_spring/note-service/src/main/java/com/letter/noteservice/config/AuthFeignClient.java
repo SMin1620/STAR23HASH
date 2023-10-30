1167160c4fb173d64b3d93c77d1e647fcb4d285f
이승민 <dltmdals1620@gmail.com>
2023-10-30 17:58:25 +0900

    Feat : 랜덤 쪽지 서버 - msa 통신 인터페이스 추가
    Change-Id: Id42b852f5d3a2039d4bb35867de83e247a2c70f5

---
+++ back/letter_spring/note-service/src/main/java/com/letter/noteservice/config/AuthFeignClient.java
@@ -0,0 +1,12 @@
+package com.letter.noteservice.config;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "auth-service")
+public interface AuthFeignClient {
+
+    @GetMapping("/api/members/test")
+    String findMember(@RequestHeader String token);
+}
