e5b5315d1b4f0433dc515308431de19452e8ac49
김예슬 <pdy051257@naver.com>
2023-10-30 21:48:20 +0900

    Design: background 설정
    Change-Id: I925f9693160d98ebf9fe3e9981bb8bc2a29255e4

--- front/frontend/src/app/today/tstorage/todayRandomStorage/page.tsx
+++ front/frontend/src/app/today/tstorage/todayRandomStorage/page.tsx
@@ -1,8 +1,9 @@
 'use client'
 import { Canvas } from '@react-three/fiber'
 import { Suspense } from 'react'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, Stars } from '@react-three/drei'
 import RandomModel from '../../../../component/today/todayStorage/random/randomPlanet/randomPlanet'
+import GradientBackground from '../../../../component/Three/three.styled'
 
 const Models = [
   {
@@ -45,11 +46,20 @@ const Models = [
 
 export default function Page() {
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
+    <GradientBackground>
       <Canvas
         style={{ width: '100%', height: '100%' }}
         camera={{ position: [-80, 100, -20], near: 0.01 }}
       >
+        <Stars
+          radius={100}
+          depth={50}
+          count={5000}
+          factor={4}
+          saturation={0}
+          fade
+          speed={1}
+        />
         <Suspense fallback={null}>
           <ambientLight intensity={0.8} />
           <spotLight position={[10, -25, -10]} angle={0.3} />
@@ -64,6 +74,6 @@ export default function Page() {
         </Suspense>
         <OrbitControls />
       </Canvas>
-    </div>
+    </GradientBackground>
   )
 }
