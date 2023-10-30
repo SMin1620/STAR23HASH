830e1bb7d2a512777cb94a2add1b502e5a59cc79
조병철 <zxas1245@naver.com>
2023-10-30 12:28:34 +0900

    Refactor: Finish modifying css
    storage 부분 필요한 tailwild 부분 빼고 styled component로 css 빼내기 완료
    
    Change-Id: I46d3702794f906f77118f20ba5bf28d549bd9225

--- front/frontend/src/app/storage/friend/[letter]/page.tsx
+++ front/frontend/src/app/storage/friend/[letter]/page.tsx
@@ -3,34 +3,32 @@ import * as l from './letter.styled'
 export default function Letter() {
   return (
     <>
-      <l.LetterBoard className="grid h-screen place-items-center bg-[length:700px] bg-[left_top_15rem]">
-        <l.LetterContainer className="grid  w-80 md:w-2/3 lg:w-2/3">
-          <l.DecoBottonWrapper className="mb-4 self-start justify-self-start">
+      <l.LetterBoard className="h-screen bg-[length:700px] bg-[left_top_15rem]">
+        <l.LetterContainer className="w-80 md:w-2/3 lg:w-2/3">
+          <l.DecoBottonWrapper>
             <l.RedDecoBotton />
             <l.YellowDecoBotton />
             <l.GreenDecoBotton />
           </l.DecoBottonWrapper>
+
           {/* other type image...sound..video.. */}
           <l.MediaWrapper>
             {/* image */}
-            <l.CustonImage
+            <l.CustomImage
               className="aspect-auto"
               src="/icons/planets/Planet-1.svg"
             />
             {/* video */}
             {/* sound */}
           </l.MediaWrapper>
-          {/* content input */}
-          <l.LetterContentWrapper className=" flex h-fit flex-col rounded-lg p-4">
-            <l.LetterContent>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
-              impedit perferendis suscipit eaque, iste dolor cupiditate
-              blanditiis ratione.
-            </l.LetterContent>
-          </l.LetterContentWrapper>
-          <l.CloseBotton className="mt-4 self-end justify-self-end">
-            닫기
-          </l.CloseBotton>
+
+          <l.LetterContent>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
+            impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis
+            ratione.
+          </l.LetterContent>
+
+          <l.CloseBotton>닫기</l.CloseBotton>
         </l.LetterContainer>
       </l.LetterBoard>
     </>
