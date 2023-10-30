8ea3d03c1e1f9498ab14ca1a114b381d3fe09f11
조병철 <zxas1245@naver.com>
2023-10-30 15:57:07 +0900

    Refactor: Separate planet card
    랜덤 페이지 행성 카드 컴포넌트  분리
    
    Change-Id: If5a08c87d9ddd02356839023803c46e36c85c44e

--- front/frontend/src/app/storage/random/page.tsx
+++ front/frontend/src/app/storage/random/page.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import * as r from './random.styled'
 
 import BackButton from '@/component/storage/BackButton'
+import PlanetCard from '@/component/storage/random/PlanetCard'
 
 export default function Ramdon() {
   return (
@@ -17,26 +18,15 @@ export default function Ramdon() {
         <r.RandomCardContainer className="h-screen ">
           <r.RandomCardWrapper className=" m-4 grid grid-cols-3 gap-x-3 gap-y-5 sm:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8">
             {/* card component start */}
-            <r.Card className="group">
-              <Link href="/storage/random/{ID}">
-                <r.CardImageWrapper className=" aspect-h-1 aspect-w-1 xl:aspect-h-8 xl:aspect-w-7 w-full group-hover:opacity-75">
-                  <r.CardStateImage
-                    className=" w-1/3 "
-                    src="/icons/Check.svg"
-                    alt="state"
-                  />
-                  <r.CustomImage
-                    src="/icons/planets/Planet-1.svg"
-                    alt="planet icon."
-                    className=" h-full w-full object-cover object-center "
-                  />
-                </r.CardImageWrapper>
-                <r.TitleWrapper>
-                  <r.CardTitle>풍성한 크리링</r.CardTitle>
-                  <r.CardUpdateDate>2023.10.12</r.CardUpdateDate>
-                </r.TitleWrapper>
-              </Link>
-            </r.Card>
+
+            <PlanetCard
+              id={Math.floor(Math.random() * 15) + 1}
+              planetNumber={Math.floor(Math.random() * 15) + 1}
+              name="김감자"
+              date="2020.02.02"
+              state="written"
+            />
+
             {/* card component end */}
           </r.RandomCardWrapper>
         </r.RandomCardContainer>
--- front/frontend/src/component/storage/storageComponent.styled.tsx
+++ front/frontend/src/component/storage/storageComponent.styled.tsx
@@ -9,3 +9,40 @@ export const BackBtnWrapper = styled.button`
   height: 2.75rem;
 `
 export const CustomImage = styled.img``
+
+export const Card = styled.button`
+  position: relative;
+`
+
+export const CardStateImage = styled(CustomImage)`
+  position: absolute;
+  top: 0;
+  right: 0;
+`
+
+export const CardImageWrapper = styled.div`
+  border=radius: 0.5rem;
+  overflow: hidden;
+`
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const CardTitle = styled.h4`
+  //   margin-top: 0.5rem;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  font-weight: bold;
+  color: white;
+`
+
+export const CardUpdateDate = styled.p`
+  font-size: 0.875rem;
+
+  font-weight: 500;
+  --tw-text-opacity: 1;
+  color: rgb(156 163 175 / var(--tw-text-opacity));
+`
