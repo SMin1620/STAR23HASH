264da0796f50f4e1bd7fcfd6cee76daebbd417a0
이승민 <dltmdals1620@gmail.com>
2023-10-30 16:30:20 +0900

    Fix : 게이트웨이 서버 - 시큐리티 수정
    Change-Id: I4aece31f2bdd0bf54e0d442d96b088c70b432a85

--- back/letter_spring/gateway/src/main/java/com/letter/gateway/config/GlobalFilter.java
+++ back/letter_spring/gateway/src/main/java/com/letter/gateway/config/GlobalFilter.java
@@ -1,41 +1,41 @@
-package com.letter.gateway.config;
-
-import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.stereotype.Component;
-import reactor.core.publisher.Mono;
-
-@Slf4j
-@Component
-public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
-    public GlobalFilter() {
-        super(Config.class);
-    }
-    @Override
-    public GatewayFilter apply(Config config) {
-        return (exchange, chain) -> {
-            ServerHttpRequest request = exchange.getRequest();
-            ServerHttpResponse response = exchange.getResponse();
-            log.info("Global Filter Message: {}", config.getMessage());
-            if (config.isShowPreLogger()) {
-                log.info("Global Filter Start: request id -> {}", request.getId());
-            }
-            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                if (config.isShowPostLogger()) {
-                    log.info("Global Filter End: response code -> {}", response.getStatusCode());
-                }
-            }));
-        };
-    }
-
-    @Data
-    public static class Config {
-        private String message;
-        private boolean showPreLogger;
-        private boolean showPostLogger;
-    }
-}
+//package com.letter.gateway.config;
+//
+//import lombok.Data;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.cloud.gateway.filter.GatewayFilter;
+//import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+//import org.springframework.http.server.reactive.ServerHttpRequest;
+//import org.springframework.http.server.reactive.ServerHttpResponse;
+//import org.springframework.stereotype.Component;
+//import reactor.core.publisher.Mono;
+//
+//@Slf4j
+//@Component
+//public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+//    public GlobalFilter() {
+//        super(Config.class);
+//    }
+//    @Override
+//    public GatewayFilter apply(Config config) {
+//        return (exchange, chain) -> {
+//            ServerHttpRequest request = exchange.getRequest();
+//            ServerHttpResponse response = exchange.getResponse();
+//            log.info("Global Filter Message: {}", config.getMessage());
+//            if (config.isShowPreLogger()) {
+//                log.info("Global Filter Start: request id -> {}", request.getId());
+//            }
+//            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+//                if (config.isShowPostLogger()) {
+//                    log.info("Global Filter End: response code -> {}", response.getStatusCode());
+//                }
+//            }));
+//        };
+//    }
+//
+//    @Data
+//    public static class Config {
+//        private String message;
+//        private boolean showPreLogger;
+//        private boolean showPostLogger;
+//    }
+//}
