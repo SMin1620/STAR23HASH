7396701412fb89740c082aed37527d5706b317fe
이승민 <dltmdals1620@gmail.com>
2023-10-30 17:57:16 +0900

    Fix : 인증 서버 - msa 통신
    Change-Id: I769685c06ce30d45f84f7af5b7df55ff0742c614

--- back/letter_spring/auth-service/src/main/java/com/letter/authservice/AuthServiceApplication.java
+++ back/letter_spring/auth-service/src/main/java/com/letter/authservice/AuthServiceApplication.java
@@ -4,23 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableFeignClients
 @SpringBootApplication
 @EnableDiscoveryClient
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
-	}
-
-	/**
-	 * 마이크로 서비스 간의 통신을 위한 세팅
-	 */
-	@Bean
-	@LoadBalanced
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
 	}
 }
