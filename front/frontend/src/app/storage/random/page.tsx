d276a42bdda26532130c5d306211892a0cf865de
조병철 <zxas1245@naver.com>
2023-10-30 10:51:05 +0900

    Feat: Add state to Random message storage
    랜덤 사용자 간 편지 주고 받을 때 편지 읽었는지 등 상태 표시 추가
    
    Change-Id: Ie2d3711a830f84ac949d389f516327ce61601298

--- front/frontend/src/app/storage/random/page.tsx
+++ front/frontend/src/app/storage/random/page.tsx
@@ -5,38 +5,50 @@ export default function Ramdon() {
   return (
     <>
       <GlobalStyle />
-      <div className="flex flex-col">
+      <r.RandomPage className="flex h-screen flex-col">
         <r.BackBtnWrapper className="flex">
-          <r.CustonImage
+          <r.CustomImage
             className="m-2 h-7 w-7 object-contain"
             src="/icons/BackBtn.svg"
           />
         </r.BackBtnWrapper>
-        <r.RandomMainTitle>
-          <r.CustonImage src="/icons/SolarSystem.svg" />
-          <p className="text-lg font-bold text-white">랜덤메시지</p>
-        </r.RandomMainTitle>
 
-        <r.RandomCardWrapper className=" m-4 grid grid-cols-3 gap-x-3 gap-y-5 sm:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8">
-          <Link href="/storage/random/history" className="min-w- group">
-            <div className=" aspect-h-1 aspect-w-1 xl:aspect-h-8 xl:aspect-w-7 w-full overflow-hidden rounded-lg">
-              <r.CustonImage
-                src="/icons/planets/Planet-1.svg"
-                alt="planet icon."
-                className=" h-full w-full object-cover object-center group-hover:opacity-75"
-              />
-            </div>
-            <div className="flex flex-col items-center">
-              <h4 className="mt-4 text-base font-bold text-white">
-                풍성한 크리링
-              </h4>
-              <p className="mt-1 text-sm font-medium text-gray-400">
-                2023.10.12
-              </p>
-            </div>
-          </Link>
-        </r.RandomCardWrapper>
-      </div>
+        <r.TitleWrapper>
+          <r.CustomImage src="/icons/SolarSystem.svg" />
+          <r.Title>랜덤메시지</r.Title>
+        </r.TitleWrapper>
+
+        <r.RandomCardContainer className="mt-4 h-screen w-full overflow-y-scroll ">
+          <r.RandomCardWrapper className=" m-4 grid grid-cols-3 gap-x-3 gap-y-5 sm:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8">
+            {/* card component start */}
+            <Link href="/storage/random/{ID}">
+              <r.Card className="group">
+                {/* card state */}
+
+                {/* end card state */}
+
+                <r.CardImageWrapper className=" aspect-h-1 aspect-w-1 xl:aspect-h-8 xl:aspect-w-7 w-full group-hover:opacity-75">
+                  <r.CardStateImage
+                    className=" w-1/3 "
+                    src="/icons/Check.svg"
+                    alt="state"
+                  />
+                  <r.CustomImage
+                    src="/icons/planets/Planet-1.svg"
+                    alt="planet icon."
+                    className=" h-full w-full object-cover object-center "
+                  />
+                </r.CardImageWrapper>
+                <r.TitleWrapper>
+                  <r.CardTitle>풍성한 크리링</r.CardTitle>
+                  <r.CardUpdateDate>2023.10.12</r.CardUpdateDate>
+                </r.TitleWrapper>
+              </r.Card>
+            </Link>
+            {/* card component end */}
+          </r.RandomCardWrapper>
+        </r.RandomCardContainer>
+      </r.RandomPage>
     </>
   )
 }
--- front/frontend/src/app/storage/random/random.styled.tsx
+++ front/frontend/src/app/storage/random/random.styled.tsx
@@ -2,22 +2,68 @@
 
 import styled from 'styled-components'
 
-export const TestDiv = styled.div`
-  color: red;
-`
-
-export const RandomCardWrapper = styled.div`
-  min-width: 20rem;
-`
+export const RandomPage = styled.div``
 
 export const BackBtnWrapper = styled.div`
   margin-top: 0.5rem;
   margin-left: 0.5rem;
 `
-export const CustonImage = styled.img``
+export const CustomImage = styled.img``
 
-export const RandomMainTitle = styled.div`
+export const TitleWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+`
+export const Title = styled.p`
+  font-size: 1.125rem;
+  line-height: 1.75rem;
+  font-weight: 700;
+  color: white;
+`
+
+export const RandomCardContainer = styled.div`
+  box-shadow: inset 0px 4px 1px rgba(0, 0, 0, 0.25);
+  background-color: rgba(155, 122, 165, 0.8);
+  overflow-y: scroll;
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`
+export const RandomCardWrapper = styled.div`
+  min-width: 20rem;
+`
+
+export const Card = styled.div`
+  position: relative;
+`
+
+export const CardStateImage = styled(CustomImage)`
+  position: absolute;
+  top: 0;
+  right: 0;
+`
+
+export const CardImageWrapper = styled.div`
+  border=radius: 0.5rem;
+  overflow: hidden;
+`
+
+export const CardTitle = styled.h4`
+  margin-top: 1rem;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  font-weight: bold;
+  color: white;
+`
+
+export const CardUpdateDate = styled.p`
+  font-size: 0.875rem;
+
+  font-weight: 500;
+  --tw-text-opacity: 1;
+  color: rgb(156 163 175 / var(--tw-text-opacity));
 `
