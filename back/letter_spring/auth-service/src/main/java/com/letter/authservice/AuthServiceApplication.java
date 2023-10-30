41d01cba4248a457155692c78c372827d7adf2b0
이승민 <dltmdals1620@gmail.com>
2023-10-30 16:31:12 +0900

    Feat : 인증 서버 - 마이크로서비스 restTemplate 로드 밸런서 빈 등록
    Change-Id: Iffac3a8845ef3c6c73175ab14476d41947f10d98

--- back/letter_spring/auth-service/src/main/java/com/letter/authservice/AuthServiceApplication.java
+++ back/letter_spring/auth-service/src/main/java/com/letter/authservice/AuthServiceApplication.java
@@ -3,6 +3,9 @@ package com.letter.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +15,12 @@ public class AuthServiceApplication {
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
+	/**
+	 * 마이크로 서비스 간의 통신을 위한 세팅
+	 */
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 }
