a1c35efd82d425cc60c1f187b8666ebb8de6c53e
이승민 <dltmdals1620@gmail.com>
2023-10-31 17:42:43 +0900

    Feat : 랜덤 쪽지 서버 - 랜덤 쪽지 생성 1차 버전
    Change-Id: I7c21d5b82100fd87a684ca7c32a295640328c96b

--- back/letter_spring/note-service/src/main/java/com/letter/noteservice/note/controller/NoteController.java
+++ back/letter_spring/note-service/src/main/java/com/letter/noteservice/note/controller/NoteController.java
@@ -1,12 +1,16 @@
 package com.letter.noteservice.note.controller;
 
+import com.letter.noteservice.common.BaseResponse;
+import com.letter.noteservice.note.dto.NoteDto;
+import com.letter.noteservice.note.entity.Note;
 import com.letter.noteservice.note.feign.AuthFeignClient;
+import com.letter.noteservice.note.mapper.NoteMapper;
+import com.letter.noteservice.note.service.NoteService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -15,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class NoteController {
 
     private final AuthFeignClient authFeignClient;
+    private final NoteService noteService;
+    private final NoteMapper noteMapper;
 
     @GetMapping("/test")
     public String test(HttpServletRequest request) {
@@ -22,4 +28,18 @@ public class NoteController {
 
         return "note test / " + request.getHeader("X-Authorization-Email") + " / " + response;
     }
+
+    /**
+     * 랜덤 쪽지 생성
+     */
+    @PostMapping("")
+    public BaseResponse noteCreate(
+            HttpServletRequest request,
+            @RequestBody NoteDto.NoteCreateReqDto dto
+            ) {
+        Note note = noteService.noteCreate(request, dto);
+        NoteDto.NoteCreateResDto noteCreateResDto = noteMapper.noteToCreateDto(note);
+        return new BaseResponse(HttpStatus.CREATED, "랜덤 쪽지 보내기 성공", noteCreateResDto);
+    }
+
 }
