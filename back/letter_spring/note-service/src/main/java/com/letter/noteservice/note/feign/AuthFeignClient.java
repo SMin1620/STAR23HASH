6b15667aaa2bb60a44d34582f608b0ffdfbd88ef
이승민 <dltmdals1620@gmail.com>
2023-10-31 17:43:20 +0900

    Feat : 랜덤 쪽지 서버 - 랜덤 쪽지 생성 api에서 open feign 설정 추가
    Change-Id: Ib11a284ab1502e7deb3b3f2611b8e0e1b7d59d2f

--- back/letter_spring/note-service/src/main/java/com/letter/noteservice/note/feign/AuthFeignClient.java
+++ back/letter_spring/note-service/src/main/java/com/letter/noteservice/note/feign/AuthFeignClient.java
@@ -1,7 +1,9 @@
 package com.letter.noteservice.note.feign;
 
+import com.letter.noteservice.note.dto.NoteDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "auth-service")
@@ -9,4 +11,7 @@ public interface AuthFeignClient {
 
     @GetMapping("/api/members/test")
     String findMember(@RequestHeader String token);
+
+    @PostMapping("/api/members/feign/random")
+    NoteDto.NoteCreateResDto findRandomMember(String phone);
 }
