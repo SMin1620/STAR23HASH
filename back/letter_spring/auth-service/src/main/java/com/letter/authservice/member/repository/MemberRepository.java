bea7607ab96828f2329da911a7cc37a1e6d85748
이승민 <dltmdals1620@gmail.com>
2023-10-31 17:45:45 +0900

    Feat : 인증 서버 - 유저 레포지토리 추가
    Change-Id: Ib43c6ce621b4d9fe6f325516ae30b2753c4f33a3

--- back/letter_spring/auth-service/src/main/java/com/letter/authservice/member/repository/MemberRepository.java
+++ back/letter_spring/auth-service/src/main/java/com/letter/authservice/member/repository/MemberRepository.java
@@ -2,6 +2,7 @@ package com.letter.authservice.member.repository;
 
 import com.letter.authservice.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -9,4 +10,9 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByPhone(String phone);
     Optional<Member> findById(Long id);
+
+    Long countBy();
+
+    @Query(value = "select * from members order by RAND() limit 1", nativeQuery = true)
+    Member findByRandom();
 }
