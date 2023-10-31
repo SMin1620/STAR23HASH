e8c718537152ec4c6785eb48c6eb7cb2e7a0979e
이승민 <dltmdals1620@gmail.com>
2023-10-31 17:57:47 +0900

    Fix : 인증 서버 - 랜덤 쪽지 보낸사람 받는사람 중복 금지 로직 추가
    Change-Id: I121f4a685fcbc02808e63eb67c8d6966fd958546

--- back/letter_spring/auth-service/src/main/java/com/letter/authservice/member/controller/MemberFeignController.java
+++ back/letter_spring/auth-service/src/main/java/com/letter/authservice/member/controller/MemberFeignController.java
@@ -27,8 +27,16 @@ public class MemberFeignController {
             @RequestBody String phone) {
         System.out.println("인증서버 - 랜덤 로직 실행 / phone : " + phone );
 
+        // 게이트웨이 검증 통과 로직
         response.setHeader("Authorization", request.getHeader("Authorization"));
-        Member member = memberRepository.findByRandom();
+        
+        // 램던 쪽지 받는 사람과 보내는 사람 중복을 금지 로직
+        Member member = new Member();
+        while (true) {
+            member = memberRepository.findByRandom();
+            if (! member.getPhone().equals(phone)) break;
+        }
+        
         return MemberFeignDto.MemberFeignRandomDto.builder()
                 .senderId(memberRepository.findByPhone(phone).get().getId())
                 .senderName("보낸 사람 닉네임")
