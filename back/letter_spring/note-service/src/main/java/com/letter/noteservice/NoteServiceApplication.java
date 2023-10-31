ef4047c60ef0fee3728b430301b3f30aa21532ee
이승민 <dltmdals1620@gmail.com>
2023-10-31 17:41:57 +0900

    Fix : 랜덤 쪽지 서버 - 어노테이션 추가
    Change-Id: Ie2b4204de5d0528176d9adf9781db5a0bb089d3c

--- back/letter_spring/note-service/src/main/java/com/letter/noteservice/NoteServiceApplication.java
+++ back/letter_spring/note-service/src/main/java/com/letter/noteservice/NoteServiceApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableJpaAuditing
+@EnableAsync
 @EnableFeignClients
 @SpringBootApplication
 @EnableDiscoveryClient
