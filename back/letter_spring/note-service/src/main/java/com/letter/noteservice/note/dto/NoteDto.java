e6af90fa7513f4d52e96bac704055d20b8cdf28e
이승민 <dltmdals1620@gmail.com>
2023-10-31 17:44:04 +0900

    Feat : 랜덤 쪽지 서버 - 랜덤 쪽지 생성 dto 및 레포지토리 추가
    Change-Id: I9c67056d7611f21573e1678f664891ae768c8182

---
+++ back/letter_spring/note-service/src/main/java/com/letter/noteservice/note/dto/NoteDto.java
@@ -0,0 +1,42 @@
+package com.letter.noteservice.note.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+/**
+ * 꿈 일기 생성 Dto
+ */
+public class NoteDto {
+
+    /**
+     * 랜덤 쪽지 생성 요청
+     */
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class NoteCreateReqDto {
+        private String content;
+    }
+
+    /**
+     * 랜덤 쪽지 생성 응답
+     */
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class NoteCreateResDto {
+        private Long senderId;
+        private String senderName;
+        private Long receiverId;
+        private String receiverName;
+        private String content;
+        private LocalDateTime createdAt;
+        private Boolean read;
+        private Boolean reply;
+    }
+}
