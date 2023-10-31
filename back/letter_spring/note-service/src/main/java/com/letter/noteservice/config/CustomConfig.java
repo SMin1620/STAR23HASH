14e4d053dbc9c5a2fed290928532614a3846babb
이승민 <dltmdals1620@gmail.com>
2023-10-31 14:43:50 +0900

    Rename : 랜덤 쪽지 서버 - 패키지 네임 변경
    Change-Id: I08bb16d0d22e80401c4fdc1f5e7657892385e04c

--- back/letter_spring/note-service/src/main/java/com/letter/noteservice/config/CustomConfig.java
+++ back/letter_spring/note-service/src/main/java/com/letter/noteservice/config/CustomConfig.java
@@ -4,7 +4,6 @@ import feign.RequestInterceptor;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
