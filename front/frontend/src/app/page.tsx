0ae93b84fcf107da20615e5e04e04a45ed8ea689
권은정 <un2642@naver.com>
2023-11-01 16:35:16 +0900

    Design: Change global background
    Change-Id: I1cc05ad45adf6057a8dd7faa205220e0bd6711e6

--- front/frontend/src/app/page.tsx
+++ front/frontend/src/app/page.tsx
@@ -3,14 +3,16 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <>
-      <div className="Title">냠냠냠`</div>
-      <div>
-        <Link href={'/storage'}>보관함으로 가깅</Link>
-      </div>
-      <div>
-        <Link className="Title" href={'/main'}>
-          Three.js Test page 로 가기
-        </Link>
+      <div className=" absolute z-10">
+        <div className="Title">냠냠냠`</div>
+        <div>
+          <Link href={'/storage'}>보관함으로 가깅</Link>
+        </div>
+        <div>
+          <Link className="Title" href={'/main'}>
+            Three.js Test page 로 가기
+          </Link>
+        </div>
       </div>
     </>
   )
---
+++ front/frontend/src/app/today/delivery/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <>
+      <div className="absolute z-10">
+        {/* <s.TestText className="text-white">로딩중우주웆우</s.TestText> */}
+      </div>
+    </>
+  )
+}
