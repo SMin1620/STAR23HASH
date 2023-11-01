0630e36ce1ae5a2afbc38ef98a557a3d86d4741b
조병철 <zxas1245@naver.com>
2023-11-01 15:29:24 +0900

    Test: Add test axios
    axios 통신 테스트 코드 작성
    
    Change-Id: Ib460427579a67ed880ce4a3237a2cd2bea701f8d

--- front/frontend/src/app/storage/loading.tsx
+++ front/frontend/src/app/storage/loading.tsx
@@ -3,7 +3,9 @@ import * as s from './storage.styled'
 export default function Loading() {
   return (
     <>
-      <s.TestText>로딩중우주웆우</s.TestText>
+      <div className="absolute z-10">
+        <s.TestText className="text-white">로딩중우주웆우</s.TestText>
+      </div>
     </>
   )
 }
