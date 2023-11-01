a3901d8c6bae4b57fbaa098af2fb38f86aca1362
김예슬 <pdy051257@naver.com>
2023-11-01 10:40:57 +0900

    Design: 카메라 각도 수정
    Change-Id: I7663061837daa2bcc5a719450d8ceb9ced23cc72

--- front/frontend/src/app/today/tstorage/todayRandomStorage/page.tsx
+++ front/frontend/src/app/today/tstorage/todayRandomStorage/page.tsx
@@ -9,38 +9,38 @@ const Models = [
   {
     name: 'planet1',
     url: '/assets/planet-1.glb',
-    scale: [6, 6, 6],
-    position: [-5, -5, 3],
+    scale: [7, 7, 7],
+    position: [-5, -3, -63],
   },
   {
     name: 'planet2',
     url: '/assets/planet-2.glb',
-    scale: [5, 5, 5],
-    position: [-3, -3, 23],
+    scale: [3, 3, 3],
+    position: [-3, -3, -43],
   },
   {
     name: 'planet3',
     url: '/assets/planet-3.glb',
-    scale: [4, 4, 4],
-    position: [-1, -1, 43],
+    scale: [5, 5, 5],
+    position: [-1, -1, -23],
   },
   {
     name: 'planet4',
     url: '/assets/planet-4.glb',
-    scale: [3, 3, 3],
-    position: [1, 1, 63],
+    scale: [5, 5, 5],
+    position: [1, 1, -3],
   },
   {
     name: 'planet5',
     url: '/assets/planet-5.glb',
-    scale: [2, 2, 2],
-    position: [3, 3, 83],
+    scale: [5, 5, 5],
+    position: [3, 3, 23],
   },
   {
     name: 'planet6',
     url: '/assets/planet-6.glb',
-    scale: [1, 1, 1],
-    position: [5, 5, 103],
+    scale: [5, 5, 5],
+    position: [5, 5, 43],
   },
 ]
 
@@ -49,20 +49,20 @@ export default function Page() {
     <GradientBackground>
       <Canvas
         style={{ width: '100%', height: '100%' }}
-        camera={{ position: [-80, 100, -20], near: 0.01 }}
+        camera={{ position: [0, 0, -100], near: 0.01 }}
       >
         <Stars
           radius={100}
-          depth={50}
-          count={5000}
-          factor={4}
-          saturation={0}
+          depth={25}
+          count={6000}
+          factor={7}
+          saturation={1}
           fade
-          speed={1}
+          speed={2}
         />
         <Suspense fallback={null}>
           <ambientLight intensity={0.8} />
-          <spotLight position={[10, -25, -10]} angle={0.3} />
+          <spotLight position={[10, 10, -10]} angle={0.3} />
           {Models.map((model) => (
             <RandomModel
               key={model.name}
