882ccf1d4763637d603a7def2d759e909dc66a15
김예슬 <pdy051257@naver.com>
2023-11-01 11:51:55 +0900

    Refactor: Planet 모델 컴컴포넌트 분리
    Change-Id: I69db060dbb2691c83c28e153ec2974c49bdca47b

--- front/frontend/src/app/today/tstorage/todayRandomStorage/page.tsx
+++ front/frontend/src/app/today/tstorage/todayRandomStorage/page.tsx
@@ -4,52 +4,53 @@ import { Suspense } from 'react'
 import { OrbitControls, Stars } from '@react-three/drei'
 import RandomModel from '../../../../component/today/todayStorage/random/randomPlanet/randomPlanet'
 import GradientBackground from '../../../../component/Three/three.styled'
+import { Models } from '@/component/today/todayStorage/random/planetModel/planetModel'
 
-const Models = [
-  {
-    name: 'planet1',
-    url: '/assets/planet-1.glb',
-    scale: [7, 7, 7],
-    position: [-5, -3, -63],
-  },
-  {
-    name: 'planet2',
-    url: '/assets/planet-2.glb',
-    scale: [3, 3, 3],
-    position: [-3, -3, -43],
-  },
-  {
-    name: 'planet3',
-    url: '/assets/planet-3.glb',
-    scale: [5, 5, 5],
-    position: [-1, -1, -23],
-  },
-  {
-    name: 'planet4',
-    url: '/assets/planet-4.glb',
-    scale: [5, 5, 5],
-    position: [1, 1, -3],
-  },
-  {
-    name: 'planet5',
-    url: '/assets/planet-5.glb',
-    scale: [5, 5, 5],
-    position: [3, 3, 23],
-  },
-  {
-    name: 'planet6',
-    url: '/assets/planet-6.glb',
-    scale: [5, 5, 5],
-    position: [5, 5, 43],
-  },
-]
+// const Models = [
+//   {
+//     name: 'planet1',
+//     url: '/assets/planet-1.glb',
+//     scale: [7, 7, 7],
+//     position: [-3, -20, -63],
+//   },
+//   {
+//     name: 'planet2',
+//     url: '/assets/planet-2.glb',
+//     scale: [3, 3, 3],
+//     position: [-1, -17, -43],
+//   },
+//   {
+//     name: 'planet3',
+//     url: '/assets/planet-3.glb',
+//     scale: [5, 5, 5],
+//     position: [1, -14, -23],
+//   },
+//   {
+//     name: 'planet4',
+//     url: '/assets/planet-4.glb',
+//     scale: [5, 5, 5],
+//     position: [3, -11, -3],
+//   },
+//   {
+//     name: 'planet5',
+//     url: '/assets/planet-5.glb',
+//     scale: [5, 5, 5],
+//     position: [5, -8, 23],
+//   },
+//   {
+//     name: 'planet6',
+//     url: '/assets/planet-6.glb',
+//     scale: [5, 5, 5],
+//     position: [7, -5, 43],
+//   },
+// ]
 
 export default function Page() {
   return (
     <GradientBackground>
       <Canvas
         style={{ width: '100%', height: '100%' }}
-        camera={{ position: [0, 0, -100], near: 0.01 }}
+        camera={{ position: [-20, 10, -100], near: 0.01 }}
       >
         <Stars
           radius={100}
