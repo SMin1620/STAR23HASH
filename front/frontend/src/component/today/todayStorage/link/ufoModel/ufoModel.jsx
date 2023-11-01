3da543135ad8367f12aa79f80d4c609f466fffa7
김예슬 <pdy051257@naver.com>
2023-11-01 10:43:33 +0900

    Feat: 링크 페이지 3d 모델 렌더링
    Change-Id: I040b37d8f636ebe7e284d72bfa1e10e566980688

---
+++ front/frontend/src/component/today/todayStorage/link/ufoModel/ufoModel.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react'
+import * as THREE from 'three'
+import { GLTFLoader } from 'three-stdlib'
+
+export default function UfoModel({ url, scale, position }) {
+  const [ufoModel, setUfoModel] = useState(null)
+
+  useEffect(() => {
+    const loader = new GLTFLoader()
+    loader.load(url, (gltf) => {
+      gltf.scene.scale.set(...scale)
+      setUfoModel(gltf.scene),
+        undefined,
+        function (error) {
+          console.log(error)
+        }
+    })
+  }, [url])
+
+  if (!ufoModel) return null
+  ufoModel.position.set(...position)
+  return <primitive object={ufoModel} />
+}
