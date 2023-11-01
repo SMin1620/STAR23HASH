1fa0f38a945a6acaf764f44d29fd8caf46be11d8
조병철 <zxas1245@naver.com>
2023-11-01 15:29:24 +0900

    Test: Add test axios
    axios 통신 테스트 코드 작성
    
    Change-Id: Ib460427579a67ed880ce4a3237a2cd2bea701f8d

--- front/frontend/src/app/layout.tsx
+++ front/frontend/src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
             position: 'absolute',
             width: '100%',
             height: '100%',
-            zIndex: -1,
+            zIndex: -999,
           }}
         />
       </body>
---
+++ front/frontend/src/app/types/types.ts
@@ -0,0 +1,6 @@
+export interface item {
+  userId: number
+  id: number
+  title: string
+  body: string
+}
