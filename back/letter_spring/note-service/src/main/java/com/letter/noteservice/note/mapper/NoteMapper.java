934b9f6114f1451a9607c790f3a137eb5b0279ae
이승민 <dltmdals1620@gmail.com>
2023-11-02 10:29:39 +0900

    Feat : 랜덤 쪽지 서버 - 쪽지 생성 및 조회 dto, entity 매퍼 구현
    Change-Id: I5851fea052218d5134ee336a1a88688147e22b7b

--- back/letter_spring/note-service/src/main/java/com/letter/noteservice/note/mapper/NoteMapper.java
+++ back/letter_spring/note-service/src/main/java/com/letter/noteservice/note/mapper/NoteMapper.java
@@ -2,11 +2,23 @@ package com.letter.noteservice.note.mapper;
 
 import com.letter.noteservice.note.dto.NoteDto;
 import com.letter.noteservice.note.entity.Note;
+import com.letter.noteservice.note.entity.Room;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 
 @Mapper(componentModel = "spring")
 public interface NoteMapper {
 
+    /**
+     *  쪽지 생성 매퍼
+     */
     NoteDto.NoteCreateResDto noteToCreateDto(Note note);
+
+    /**
+     * 쪽지 모든 날짜 목록 조회 매퍼
+     */
+    List<NoteDto.NoteListResDto> noteToListDto(List<Room> noteList);
+
 }
