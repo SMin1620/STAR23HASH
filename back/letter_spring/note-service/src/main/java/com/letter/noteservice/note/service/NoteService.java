d7134dcec21500fbd9239bebb0928bb0476f3de7
이승민 <dltmdals1620@gmail.com>
2023-11-02 10:28:58 +0900

    Feat : 랜덤 쪽지 서버 - 쪽지 생성 및 조회 비즈니스 로직 구현
    Change-Id: Ie6191f917993f5226aa69e45f06d105262cebbbf

--- back/letter_spring/note-service/src/main/java/com/letter/noteservice/note/service/NoteService.java
+++ back/letter_spring/note-service/src/main/java/com/letter/noteservice/note/service/NoteService.java
@@ -17,6 +17,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -52,6 +54,9 @@ public class NoteService {
                 .senderName(data.getSenderName())
                 .receiverId(data.getReceiverId())
                 .receiverName(data.getReceiverName())
+                .read(false)
+                .reply(false)
+                .store(false)
                 .createdAt(LocalDateTime.now())
                 .build();
         roomRepository.save(room);
@@ -72,6 +77,115 @@ public class NoteService {
 
         return note;
 
+    }
 
+    /**
+     * 쪽지 답장
+     */
+    @Transactional
+    public Note noteReply(
+            HttpServletRequest request,
+            NoteDto.NoteCreateReqDto dto,
+            Long roomId
+    ) {
+        // 요청 헤더에서 멤버 유니크 변수로 저장
+        String phone = request.getHeader("Member-Authorization-Phone");
+        System.out.println("phone >>> " + phone);
+
+        Room room = roomRepository.findById(roomId)
+                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.ROOM_NOT_FOUND));
+
+        Note note = Note.builder()
+                .senderId(room.getReceiverId())
+                .senderName(room.getReceiverName())
+                .receiverId(room.getSenderId())
+                .receiverName(room.getSenderName())
+                .content(dto.getContent())
+                .createdAt(room.getCreatedAt())
+                .read(false)
+                .reply(false)
+                .room(room)
+                .build();
+
+        noteRepository.save(note);
+
+        return note;
+    }
+
+    /**
+     * 쪽지 방의 모든 쪽지들 목록 조회
+     */
+    public List<NoteDto.NoteListResDto> noteList(
+            HttpServletRequest request,
+            Long roomId
+    ) {
+        // 요청 헤더에서 멤버 유니크 변수로 저장
+        String phone = request.getHeader("Member-Authorization-Phone");
+        System.out.println("phone >>> " + phone);
+
+        // 인증 서버에서 유저 id 가져오기
+        Long memberId = authFeignClient.findMemberId(phone);
+
+        // 각 쪽지의 읽음 여부 처리 확인
+        List<Note> noteList = noteRepository.findAllByNote(roomId);
+        List<NoteDto.NoteListResDto> noteListResDtos = new ArrayList<>();
+        for (Note note : noteList) {
+            NoteDto.NoteListResDto dto = NoteDto.NoteListResDto.builder()
+                    .id(note.getId())
+                    .senderId(note.getSenderId())
+                    .senderName(note.getSenderName())
+                    .receiverId(note.getReceiverId())
+                    .receiverName(note.getReceiverName())
+                    .content(note.getContent())
+                    .createdAt(note.getCreatedAt())
+                    .build();
+
+            if (note.getSenderId() == memberId) dto.setSend(true);
+            else dto.setSend(false);
+
+            if (note.getReceiverId() == memberId && ! note.getRead()) dto.setRead(false);
+            else dto.setRead(true);
+
+            noteListResDtos.add(dto);
+        }
+
+        return noteListResDtos;
+    }
+
+
+    /**
+     * 쪽지 상세 조회
+     * -> 읽음 여부 처리
+     */
+    @Transactional
+    public NoteDto.NoteDetailResDto noteDetail(HttpServletRequest request, Long noteId) {
+        // 요청 헤더에서 멤버 유니크 변수로 저장
+        String phone = request.getHeader("Member-Authorization-Phone");
+        System.out.println("phone >>> " + phone);
+
+        // 인증 서버에서 유저 id 가져오기
+        Long memberId = authFeignClient.findMemberId(phone);
+
+        Note note = noteRepository.findById(noteId)
+                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.NOTE_NOT_FOUND));
+
+        // 쪽지 읽음 처리 여부
+        if (! note.getRead() && note.getSenderId() != memberId) updateNoteReadStatus(note);
+
+        return NoteDto.NoteDetailResDto.builder()
+                .id(note.getId())
+                .senderId(note.getSenderId())
+                .senderName(note.getSenderName())
+                .receiverId(note.getReceiverId())
+                .receiverName(note.getReceiverName())
+                .content(note.getContent())
+                .createdAt(note.getCreatedAt())
+                .build();
+
+    }
+
+    @Transactional
+    public void updateNoteReadStatus(Note note) {
+        note.setRead(true);
     }
 }
