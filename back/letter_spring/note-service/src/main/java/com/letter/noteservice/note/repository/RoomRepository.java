4cada3c806d3a55a160898ec58b5f3e9bc8a93d8
이승민 <dltmdals1620@gmail.com>
2023-11-02 10:31:12 +0900

    Feat : 랜덤 쪽지 서버 - 쪽지 방 조회 매퍼 및 디비 접근 로직 구현
    Change-Id: I11ad2332c0423b6c8678118ee0a47467f6ef2888

---
+++ back/letter_spring/note-service/src/main/java/com/letter/noteservice/note/repository/RoomRepository.java
@@ -0,0 +1,25 @@
+package com.letter.noteservice.note.repository;
+
+import com.letter.noteservice.note.entity.Note;
+import com.letter.noteservice.note.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+    Optional<Room> findBySenderIdAndReceiverId(Long senderId, Long ReceiverId);
+
+    /**
+     * 쪽지 방에서 쪽지 목록 조회
+     */
+    @Query(value = "select r from Room r where r.senderId = :id")
+    List<Room> findAllBySenderIdOrReceiverID(Long id);
+
+    /**
+     * 쪽지 방 목록 조회
+     */
+    @Query(value = "select r from Room r where r.senderId = :memberId or r.receiverId = :memberId")
+    List<Room> findAllByRoom(Long memberId);
+}
