da3acd2e9474fdbdf9e113f131abc3ffa37de113
이승민 <dltmdals1620@gmail.com>
2023-11-02 14:33:00 +0900

    Feat : 인증 서버 - cors 수정
    Change-Id: I8850d9aa8b8df17ee40c5a14fae7ccc070804973

--- back/letter_spring/auth-service/src/main/java/com/letter/authservice/config/SecurityConfig.java
+++ back/letter_spring/auth-service/src/main/java/com/letter/authservice/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
-    private final CorsConfig corsConfig;
+//    private final CorsConfig corsConfig;
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
@@ -50,8 +50,8 @@ public class SecurityConfig {
         http.httpBasic().disable()
                 .csrf().disable()
                 .headers().frameOptions().disable()// crsf 보안 X (rest api)
-                .and()
-                .apply(new MyCustomDsl())
+//                .and()
+//                .apply(new MyCustomDsl())
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); // jwt token으로 인증 > 세션 필요없음
 
@@ -89,15 +89,15 @@ public class SecurityConfig {
     }
 
     // 커스텀 필터 추가를 여기서 처리하기
-    public class MyCustomDsl extends AbstractHttpConfigurer<MyCustomDsl, HttpSecurity> {
-        @Override
-        public void configure(HttpSecurity http) throws Exception {
-            AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
-            http
-                    // cors 오류를 해결하기 위해 Controller 에 @CrossOrigin 을 붙여주는 방법도 있지만
-                    // 이 방식은 필터 추가와 다르게 인증이 필요 없는 url 만 처리해줌
-                    .addFilter(corsConfig.corsFilter()); // cors 에 대해 허락하는 필터
-            // .addFilter(new JwtAuthorizationFilter(authenticationManager));
-        }
-    }
+//    public class MyCustomDsl extends AbstractHttpConfigurer<MyCustomDsl, HttpSecurity> {
+//        @Override
+//        public void configure(HttpSecurity http) throws Exception {
+//            AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
+//            http
+//                    // cors 오류를 해결하기 위해 Controller 에 @CrossOrigin 을 붙여주는 방법도 있지만
+//                    // 이 방식은 필터 추가와 다르게 인증이 필요 없는 url 만 처리해줌
+//                    .addFilter(corsConfig.corsFilter()); // cors 에 대해 허락하는 필터
+//            // .addFilter(new JwtAuthorizationFilter(authenticationManager));
+//        }
+//    }
 }
