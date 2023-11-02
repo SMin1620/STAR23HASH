322186a1ec8376320b788bc7785e45a2ba743652
이승민 <dltmdals1620@gmail.com>
2023-11-02 11:54:44 +0900

    Fix : 랜덤 쪽지 서버 - 쪽지 읽음 여부 처리 Optional 처리
    Change-Id: I6244b04c8bfe107b32740b27b564674406d4d606

--- back/letter_spring/note-service/src/main/java/com/letter/noteservice/note/service/NoteService.java
+++ back/letter_spring/note-service/src/main/java/com/letter/noteservice/note/service/NoteService.java
@@ -110,7 +110,7 @@ public class NoteService {
         noteRepository.save(note);
 
         // 이전 쪽지 답장 여부 처리
-        Note preNote = noteRepository.findByPreNote(roomId, memberId)
+        Note preNote = noteRepository.findTopByRoomIdAndReceiverIdOrderByCreatedAtDesc(roomId, memberId)
                 .orElseThrow(() -> new BusinessLogicException(ExceptionCode.NOTE_NOT_FOUND));
         preNote.setReply(true);
 
