4ff8238f9b0a2c2b907df14c5d75b5b24d182351
이승민 <dltmdals1620@gmail.com>
2023-11-02 10:29:19 +0900

    Feat : 랜덤 쪽지 서버 - 쪽지 생성 및 조회 데이터베이스 접근 로직 구현
    Change-Id: I2fe5bf52db57905ebbf3be02a4e60c0a6177413e

--- back/letter_spring/note-service/src/main/java/com/letter/noteservice/note/repository/NoteRepository.java
+++ back/letter_spring/note-service/src/main/java/com/letter/noteservice/note/repository/NoteRepository.java
@@ -3,6 +3,23 @@ package com.letter.noteservice.note.repository;
 import com.letter.noteservice.note.entity.Note;
 import org.aspectj.weaver.ast.Not;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
+
+//    @Query(value = "select n from Note n where n.room.id = :roomId and n.receiverId = :memberId")
+//    List<Note> findByLastNote(Long roomId, Long memberId);
+
+    @Query(value = "select n from Note n where n.room.id =:roomId and n.receiverId =:memberId and n.read = false")
+    Optional<Note> findRoomByNote(Long roomId, Long memberId);
+
+    /**
+     * 쪽지 방의 쪽지 목록 조회
+     */
+    @Query(value = "select n from Note n where n.room.id =:roomId")
+    List<Note> findAllByNote(Long roomId);
+
 }
