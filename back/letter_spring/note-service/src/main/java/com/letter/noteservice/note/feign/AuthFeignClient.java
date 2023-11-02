ee581a4a1f645e7631266bd859eb8c0e0def1144
이승민 <dltmdals1620@gmail.com>
2023-11-02 10:30:01 +0900

    Feat : 랜덤 쪽지 서버 - 쪽지 생성 로직에서 인증 서버 접근 API 구현
    Change-Id: I081257d3ffd22ffff5ea6a793d98f5bfb42c55c8

--- back/letter_spring/note-service/src/main/java/com/letter/noteservice/note/feign/AuthFeignClient.java
+++ back/letter_spring/note-service/src/main/java/com/letter/noteservice/note/feign/AuthFeignClient.java
@@ -1,8 +1,10 @@
 package com.letter.noteservice.note.feign;
 
 import com.letter.noteservice.note.dto.NoteDto;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
@@ -17,5 +19,11 @@ public interface AuthFeignClient {
      */
     @PostMapping("/api/members/feign/random")
     NoteDto.NoteCreateResDto findRandomMember(String phone);
+
+    /**
+     * 유저 id 가져오기
+     */
+    @GetMapping("/api/members/feign/{phone}")
+    Long findMemberId(@PathVariable(name = "phone") String phone);
 
 }
