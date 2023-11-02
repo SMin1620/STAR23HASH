ef27dad80d0ad1c0492b63ea4f874300c5d94bf0
이승민 <dltmdals1620@gmail.com>
2023-11-02 10:30:57 +0900

    Feat : 랜덤 쪽지 서버 - 쪽지 방 조회 비즈니스 로직 구현
    Change-Id: I39cfc4c3fbcd715fafdf05376ba7cc4e2db4398e

---
+++ back/letter_spring/note-service/src/main/java/com/letter/noteservice/note/service/RoomService.java
@@ -0,0 +1,93 @@
+package com.letter.noteservice.note.service;
+
+import com.letter.noteservice.note.dto.RoomDto;
+import com.letter.noteservice.note.entity.Note;
+import com.letter.noteservice.note.entity.Room;
+import com.letter.noteservice.note.feign.AuthFeignClient;
+import com.letter.noteservice.note.repository.NoteRepository;
+import com.letter.noteservice.note.repository.RoomRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.weaver.ast.Not;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class RoomService {
+
+    private final RoomRepository roomRepository;
+    private final NoteRepository noteRepository;
+    private final AuthFeignClient authFeignClient;
+
+
+    /**
+     * 쪽지 방 목록 조회
+     */
+    public List<RoomDto.RoomListResDto> roomList(HttpServletRequest request) {
+        // 요청 헤더에서 멤버 유니크 변수로 저장
+        String phone = request.getHeader("Member-Authorization-Phone");
+        System.out.println("phone >>> " + phone);
+
+        // 인증 서버에서 유저 id 가져오기
+        Long memberId = authFeignClient.findMemberId(phone);
+
+        List<Room> roomList = roomRepository.findAllByRoom(memberId);
+        List<RoomDto.RoomListResDto> roomListResDtos = new ArrayList<>();
+        for (Room room : roomList) {
+            RoomDto.RoomListResDto roomDto = RoomDto.RoomListResDto.builder()
+                    .id(room.getId())
+                    .senderId(room.getSenderId())
+                    .senderName(room.getSenderName())
+                    .receiverId(room.getReceiverId())
+                    .receiverName(room.getReceiverName())
+                    .createdAt(room.getCreatedAt())
+                    .build();
+
+
+            Optional<Note> dto = noteRepository.findRoomByNote(room.getId(), memberId);
+            if (dto.isEmpty()) roomDto.setRead(true);
+            else roomDto.setRead(false);
+
+            roomListResDtos.add(roomDto);
+        }
+
+        return roomListResDtos;
+    }
+
+
+    /**
+     * 쪽지 방 상세 조회
+     */
+    public Room roomDetail(HttpServletRequest request, Long roomId) {
+        // 요청 헤더에서 멤버 유니크 변수로 저장
+        String phone = request.getHeader("Member-Authorization-Phone");
+        System.out.println("phone >>> " + phone);
+
+        // 인증 서버에서 유저 id 가져오기
+        Long memberId = authFeignClient.findMemberId(phone);
+
+        Room room = roomRepository.findById(roomId).get();
+
+        // 상세 조회시 모든 쪽지 읽음 처리
+//        List<Note> noteList = noteRepository.findByLastNote(roomId, memberId);
+//        for (Note note: noteList) {
+//            System.out.println("note >>> " + note.toString());
+//            if (! note.getRead()) updateRoomReadStatus(room);
+//        }
+
+        return room;
+    }
+
+    @Transactional
+    public void updateRoomReadStatus(Room room) {
+        room.setRead(false);
+    }
+}
