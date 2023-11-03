9f512b655ca8ef08dbdf15bd453aaf4a5633443b
권은정 <un2642@naver.com>
2023-11-03 17:19:02 +0900

    Feat: 카메라 이동 효과
    Change-Id: I137d218f10f8e65a15df6837bce92086b941407c

--- front/frontend/src/app/main/layout.tsx
+++ front/frontend/src/app/main/layout.tsx
@@ -1,3 +1,5 @@
+'use client'
+import React, { useState, useEffect, Suspense } from 'react'
 import Three2 from '../../component/Three/three2'
 export default function Main({ children }: { children: React.ReactNode }) {
   return (
@@ -5,7 +7,7 @@ export default function Main({ children }: { children: React.ReactNode }) {
       <div className="flex items-center justify-center">
         <div className="absolute z-10">
           <div className="flex items-center justify-center pt-20 text-white">
-            {children}
+            <div>{children}</div>
           </div>
         </div>
       </div>
