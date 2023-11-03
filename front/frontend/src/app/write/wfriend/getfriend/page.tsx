1242ded631c01fff5ad39e874483b2cec36f08ab
조은정 <ejjeongs@naver.com>
2023-11-03 16:59:57 +0900

    Fix: Modify flutter page router && Add get contacts css
    Change-Id: Idd69d3fa9a6c8ec1d1ee3c5c2d0b7e5fc79f327d

--- front/frontend/src/app/write/wfriend/getfriend/page.tsx
+++ front/frontend/src/app/write/wfriend/getfriend/page.tsx
@@ -1,10 +1,52 @@
+'use client'
+import { useState } from 'react'
 import Link from 'next/link'
 import GlobalStyle from '../../GlobalStyles'
 import * as st from './getfriend.styled'
 import * as stt from '@/component/common/write_layout/write_layout.styled'
+import { useRouter } from 'next/navigation'
+
+type Contact = {
+  이름: string
+  연락처: string
+}
 
 export default function WriteFriend() {
-  const choice = () => {}
+  const [phone, setPhone] = useState('')
+  const [selectedContactIndex, setSelectedContactIndex] = useState(0)
+
+  const router = useRouter()
+  const contacts: Contact[] = [
+    { 이름: '조은정0', 연락처: '01012341234' },
+    { 이름: '조은정1', 연락처: '01012341234' },
+    { 이름: '조은정2', 연락처: '01012341234' },
+    { 이름: '조은정3', 연락처: '01012341234' },
+    { 이름: '조은정4', 연락처: '01012341234' },
+    { 이름: '조은정5', 연락처: '01012341234' },
+    { 이름: '조은정6', 연락처: '01012341234' },
+    { 이름: '조은정7', 연락처: '01012341234' },
+    { 이름: '조은정8', 연락처: '01012341234' },
+    { 이름: '조은정9', 연락처: '01012341234' },
+    { 이름: '조은정10', 연락처: '01012341234' },
+    { 이름: '조은정11', 연락처: '01012341234' },
+    { 이름: '조은정12', 연락처: '01012341234' },
+    { 이름: '조은정13', 연락처: '01012341234' },
+    { 이름: '조은정14', 연락처: '01012341234' },
+  ]
+
+  const selectContact = (index: number) => {
+    console.log(contacts[index].이름, contacts[index].연락처)
+    setPhone(contacts[index].연락처)
+    setSelectedContactIndex(index)
+  }
+
+  const handleSendClick = async () => {
+    if (phone === '') {
+      alert('보낼 사람을 선택해주세요!')
+    } else {
+      router.push(`/write/wfriend/input?phone=${phone}`)
+    }
+  }
   return (
     <>
       <GlobalStyle />
@@ -15,13 +57,23 @@ export default function WriteFriend() {
             <stt.InnerCircle2></stt.InnerCircle2>
             <stt.InnerCircle3></stt.InnerCircle3>
           </stt.InnerCircle>
-          <st.ContentBox>
-            <st.WhoSend>당신의 진심을 전달해보세요</st.WhoSend>
-            <st.WhoSend>대충 연락처 불러오는부분</st.WhoSend>
-          </st.ContentBox>
-          <Link href="/write/wfriend/input">
-            <stt.button>편지쓰기</stt.button>
-          </Link>
+
+          <st.ContactBox>
+            {contacts.map((contact, index) => (
+              <st.ContactObject
+                key={index}
+                onClick={() => selectContact(index)}
+                isSelected={selectedContactIndex === index}
+              >
+                <st.ContactName>{contact.이름}</st.ContactName>
+                <st.ContactPhone>전화번호 : {contact.연락처}</st.ContactPhone>
+              </st.ContactObject>
+            ))}
+          </st.ContactBox>
+
+          <stt.EmptyDiv>
+            <stt.button onClick={handleSendClick}>편지쓰기</stt.button>
+          </stt.EmptyDiv>
         </stt.SendBox>
       </stt.SendBoxDiv>
     </>
