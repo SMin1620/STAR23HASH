b9681dea082ac917e26c3832e248f3007fc4f53b
이승민 <dltmdals1620@gmail.com>
2023-11-03 11:12:31 +0900

    Feat : 인증 서버 - 롤링페이퍼 엔티티 설계
    Change-Id: I00fcc19259746accd443ab3aeeb64dabebb9457e

---
+++ back/letter_spring/auth-service/src/main/java/com/letter/authservice/member/entity/Paper.java
@@ -0,0 +1,30 @@
+package com.letter.authservice.member.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "papers")
+public class Paper {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "paper_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "roll_id")
+    private Roll roll;
+
+    @Column(nullable = false, length = 100)
+    private String content;
+
+    private LocalDateTime createdAt;
+}
