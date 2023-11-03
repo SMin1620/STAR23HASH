5159e7847b629e2f70c90b570dff3b4d6900b2fb
김예슬 <pdy051257@naver.com>
2023-11-03 12:40:18 +0900

    Feat: 행성 클릭 이벤트 추가
    Change-Id: I158f41969c81eabe2c22dfa8ea0b82fdb70f77c1

--- front/frontend/src/component/today/todayStorage/random/planetModel/planetModel.jsx
+++ front/frontend/src/component/today/todayStorage/random/planetModel/planetModel.jsx
@@ -2,31 +2,31 @@ export const Models = [
   {
     name: 'planet1',
     url: '/assets/planet-1.glb',
-    scale: [23, 23, 23],
+    scale: [30, 30, 30],
   },
   {
     name: 'planet2',
     url: '/assets/planet-2.glb',
-    scale: [8, 8, 8],
+    scale: [13, 13, 13],
   },
   {
     name: 'planet3',
     url: '/assets/planet-3.glb',
-    scale: [18, 18, 18],
+    scale: [25, 25, 25],
   },
   {
     name: 'planet4',
     url: '/assets/planet-4.glb',
-    scale: [18, 18, 18],
+    scale: [25, 25, 25],
   },
   {
     name: 'planet5',
     url: '/assets/planet-5.glb',
-    scale: [18, 18, 18],
+    scale: [25, 25, 25],
   },
   {
     name: 'planet6',
     url: '/assets/planet-6.glb',
-    scale: [18, 18, 18],
+    scale: [25, 25, 25],
   },
 ]
