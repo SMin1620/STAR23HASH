7dc9f9c0787f426fa64d761a71c3ab7049f8afaf
김예슬 <pdy051257@naver.com>
2023-11-05 04:58:40 +0900

    Rename: 오늘의 랜덤 보관함 경로 수정
    Change-Id: I195b4dbfaee02edb95447b3d5227d1bc61b195be

--- front/frontend/src/app/today/storage/Random/page.tsx
+++ front/frontend/src/app/today/storage/Random/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { Canvas, useThree } from '@react-three/fiber'
-import { Suspense, useEffect, useRef, useMemo } from 'react'
+import { Suspense, useEffect, useRef } from 'react'
 import { OrbitControls, Stars } from '@react-three/drei'
 import RandomModel from '../../../../component/today/todayStorage/random/randomPlanet/randomPlanet'
 import GradientBackground from '../../../../component/Three/three.styled'
@@ -10,8 +10,6 @@ import * as THREE from 'three'
 import RandomPlanet from '../../../../component/today/todayStorage/random/randomPlanet/randomPlanet'
 import { useGesture } from '@use-gesture/react'
 import Light from '@/component/today/todayStorage/light/light'
-import { useSpring, animated as a } from '@react-spring/web'
-import { useDrag } from '@use-gesture/react'
 
 const totalAmount = 30
 
@@ -47,44 +45,76 @@ export default function TodayRandomStorage() {
     .fill(null)
     .map(() => ({ current: null as THREE.Object3D | null }))
 
+  const bind = useGesture({
+    onDrag: ({ offset: [x, y], direction, movement: [mx, my] }) => {
+      console.log(direction)
+      console.log('offset', x, y)
+      console.log('movement', x, y)
+
+      planeRefs.forEach((ref) => {
+        if (ref.current) {
+          if (my > 0) {
+            ref.current.position.y += y * -0.0001
+            ref.current.position.z += y * -0.0012
+          } else if (my < 0) {
+            ref.current.position.y += y * 0.0001
+            ref.current.position.z += y * 0.0012
+          }
+          // ref.current.position.x += x * 0.05
+        }
+      })
+    },
+  })
   return (
-    <Canvas
-      style={{ width: '100%', height: '100%', position: 'absolute' }}
-      camera={{
-        position: [-22, 50, -100],
-        // rotation: [0, Math.PI / 8, 0],
-        near: 0.01,
-      }}
-    >
-      <Light />
-      <Suspense fallback={null}>
-        <ambientLight intensity={0.5} />
-        {Array.from({ length: totalAmount }).map((_, index) => {
-          const model = Models[index % Models.length]
-          const startPostion = [-3, -20, -63]
-          const position = [
-            startPostion[0],
-            startPostion[1] + 10 * index,
-            startPostion[2] + 120 * index,
-          ]
-          return (
-            <RandomPlanet
-              key={index}
-              url={model.url}
-              scale={model.scale}
-              position={position}
-              mesh={planeRefs[index]}
-            />
-          )
-        })}
-        <RocketModel
-          url="/assets/rocket-1.glb"
-          scale={[10, 10, 10]}
-          position={[-23, 73, 85]}
-          rotation={[Math.PI / 4.5, Math.PI, Math.PI / -8]}
+    <GradientBackground style={{ touchAction: 'none' }}>
+      <Canvas
+        style={{ width: '100%', height: '100%' }}
+        camera={{
+          position: [-22, 50, -100],
+          // rotation: [0, Math.PI / 8, 0],
+          near: 0.01,
+        }}
+        {...bind()}
+      >
+        <Stars
+          radius={100}
+          depth={25}
+          count={6000}
+          factor={8}
+          saturation={1}
+          fade
+          speed={2}
         />
-      </Suspense>
-      {/* <OrbitControls /> */}
-    </Canvas>
+        <Light />
+        <Suspense fallback={null}>
+          <ambientLight intensity={0.5} />
+          {Array.from({ length: totalAmount }).map((_, index) => {
+            const model = Models[index % Models.length]
+            const startPostion = [-3, -20, -63]
+            const position = [
+              startPostion[0],
+              startPostion[1] + 10 * index,
+              startPostion[2] + 120 * index,
+            ]
+            return (
+              <RandomPlanet
+                key={index}
+                url={model.url}
+                scale={model.scale}
+                position={position}
+                mesh={planeRefs[index]}
+              />
+            )
+          })}
+          <RocketModel
+            url="/assets/rocket-1.glb"
+            scale={[10, 10, 10]}
+            position={[-23, 73, 85]}
+            rotation={[Math.PI / 4.5, Math.PI, Math.PI / -8]}
+          />
+        </Suspense>
+        {/* <OrbitControls /> */}
+      </Canvas>
+    </GradientBackground>
   )
 }
