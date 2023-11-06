61a5b597fab3417e30547f44a7d2a31fee5d0ef6
이승민 <dltmdals1620@gmail.com>
2023-11-06 17:15:52 +0900

    Feat : 인증 서버 - 작성 제한 스케줄러
    Change-Id: Ib394068e480a6139f859f684c16d603eccbde387

--- back/letter_spring/auth-service/src/main/java/com/letter/authservice/member/repository/MemberRepository.java
+++ back/letter_spring/auth-service/src/main/java/com/letter/authservice/member/repository/MemberRepository.java
@@ -4,6 +4,7 @@ import com.letter.authservice.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -18,4 +19,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findByRandom();
 
 
+    /**
+     * 작성 제한 스케줄러
+     */
+    List<Member> findAllByIsWriteTrue();
 }
--- back/letter_spring/auth-service/src/main/java/com/letter/authservice/member/service/SchedulerService.java
+++ back/letter_spring/auth-service/src/main/java/com/letter/authservice/member/service/SchedulerService.java
@@ -1,22 +1,24 @@
-package com.letter.authservice.member.controller;
+package com.letter.authservice.member.service;
 
 import com.letter.authservice.member.entity.Member;
 import com.letter.authservice.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Component
 @RequiredArgsConstructor
-public class Scheduler {
+public class SchedulerService {
 
     private final MemberRepository memberRepository;
 
+    @Transactional
     @Scheduled(cron = "0 0 18 * * ?")
     public void reset() {
-        List<Member> memberList = memberRepository.findAll();
+        List<Member> memberList = memberRepository.findAllByIsWriteTrue();
         for (Member member : memberList) member.setIsWrite(false);
     }
 }
