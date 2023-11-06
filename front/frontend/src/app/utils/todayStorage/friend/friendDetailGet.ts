833beb0b677cb17cdb8ec031fd12d8efe9d8008e
김예슬 <pdy051257@naver.com>
2023-11-06 12:36:10 +0900

    Feat : 오늘의 친구 보관함-api 연결
    Change-Id: I9ef9417170b07ff32bc9b5c4164537853d33c12d

---
+++ front/frontend/src/app/utils/todayStorage/friend/friendDetailGet.ts
@@ -0,0 +1,15 @@
+import { AxiosResponse } from 'axios'
+import AuthAxios from '../../storage/AuthAxios'
+
+const DOMAIN = process.env.NEXT_PUBLIC_API_URL
+
+export const friendDetailGet = async (id: number): Promise<any> => {
+  try {
+    const response: AxiosResponse = await AuthAxios({
+      method: 'get',
+      url: `${DOMAIN}/api/notes/${id}`,
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
