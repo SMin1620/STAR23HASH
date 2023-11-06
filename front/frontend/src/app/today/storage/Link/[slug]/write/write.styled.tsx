23c2ffb6cd9ad2866baa4e601711a003be2baae0
김예슬 <pdy051257@naver.com>
2023-11-07 04:44:49 +0900

    Design : 롤링페이퍼 편지 레이아웃
    Change-Id: Iaa1d8692a608b911156697afb92733e67837d8f5

---
+++ front/frontend/src/app/today/storage/Link/[slug]/write/write.styled.tsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+export const SendBoxDiv = styled.div`
+  background-color: #c5e7ac;
+`
+export const SendBox = styled.div`
+  min-width: 18rem;
+  height: 35rem;
+  max-width: 45rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+`
+export const AstronautContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`
+
+export const AstronautImages = styled.div`
+  margin-top: 90px;
+  display: flex;
+`
+
+export const AstronautImg = styled.img`
+  margin-left: 10px;
+  width: 80px;
+  height: 80px;
+`
