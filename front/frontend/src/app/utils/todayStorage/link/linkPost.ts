d531060a919119859ef979833226ca8ddd778947
김예슬 <pdy051257@naver.com>
2023-11-07 04:09:01 +0900

    Feat : headers 추가
    Change-Id: I44a91901c57cb3b1ab1997fa1b7657e5124dd431

--- front/frontend/src/app/utils/todayStorage/link/linkPost.ts
+++ front/frontend/src/app/utils/todayStorage/link/linkPost.ts
@@ -8,18 +8,19 @@ const axiosInstance = axios.create({
 export const linkPost = async (
   id: number,
   content: string,
+  iconUrl: string,
 ): Promise<AxiosResponse> => {
   try {
     const response: AxiosResponse = await axiosInstance.post(
       `${DOMAIN}/api/rolls/${id}`,
       {
         content: content,
+        iconUrl: iconUrl,
       },
     )
     if (!response || response.status !== 200) {
       throw new Error('에러')
     }
-    console.log(response)
     return response.data
   } catch (error) {
     throw new Error('네트워크 오류')
