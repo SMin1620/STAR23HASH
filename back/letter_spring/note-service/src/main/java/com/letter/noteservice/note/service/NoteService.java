8d84d7dec2ebf874d35343617f33d3b2765ee77b
이승민 <dltmdals1620@gmail.com>
2023-11-06 23:39:37 +0900

    Fix : 랜덤 쪽지 서버 - 쪽지 답장 처리 수정
    Change-Id: I98b318765f91f9242989c8b2978c18c22b3180c4

--- back/letter_spring/note-service/src/main/java/com/letter/noteservice/note/service/NoteService.java
+++ back/letter_spring/note-service/src/main/java/com/letter/noteservice/note/service/NoteService.java
@@ -131,7 +131,7 @@ public class NoteService {
         noteRepository.save(note);
 
         // 이전 쪽지 답장 여부 처리
-        Note preNote = noteRepository.findTopByRoomIdAndReceiverIdOrSenderIdOrderByCreatedAtDesc(roomId, memberId, memberId)
+        Note preNote = noteRepository.findTopByRoomIdAndReceiverIdOrderByCreatedAtDesc(roomId, memberId)
                 .orElseThrow(() -> new BusinessLogicException(ExceptionCode.NOTE_NOT_FOUND));
         preNote.setIsReply(true);
 
