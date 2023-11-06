4ca0306e26ee490a1168b1b826b06b4b52508638
김예슬 <pdy051257@naver.com>
2023-11-06 12:23:30 +0900

    Fix: 빌드 에러 수정
    Change-Id: I3bc3f6be4c0315f38910cc85e027c7b36516f856

--- front/frontend/src/app/today/storage/Link/[slug]/write/page.tsx
+++ front/frontend/src/app/today/storage/Link/[slug]/write/page.tsx
@@ -1,37 +1,37 @@
 'use client'
-import styled from 'styled-components'
+// import styled from 'styled-components'
 
-export const Wbutton = styled.button`
-  box-shadow: 0px 4px 2px
-    rgba(113.9000129699707, 97.38451838493347, 98.1026342511177, 1);
-  background-color: #947d7e;
-  width: 80%;
-  margin-bottom: 20px;
-  height: 45px;
-  border-radius: 20px;
-  color: white;
-  font-size: 16px;
-  font-weight: bold;
-`
+// export const Wbutton = styled.button`
+//   box-shadow: 0px 4px 2px
+//     rgba(113.9000129699707, 97.38451838493347, 98.1026342511177, 1);
+//   background-color: #947d7e;
+//   width: 80%;
+//   margin-bottom: 20px;
+//   height: 45px;
+//   border-radius: 20px;
+//   color: white;
+//   font-size: 16px;
+//   font-weight: bold;
+// `
 
-import {
-  InnerCircle,
-  InnerCircle1,
-  InnerCircle2,
-  InnerCircle3,
-  SendBox,
-  SendBoxDiv,
-} from '@/component/common/write_layout/write_layout.styled'
+// import {
+//   InnerCircle,
+//   InnerCircle1,
+//   InnerCircle2,
+//   InnerCircle3,
+//   SendBox,
+//   SendBoxDiv,
+// } from '@/component/common/write_layout/write_layout.styled'
 
 export default function LinkWrite() {
   return (
-    <SendBox>
-      <InnerCircle>
-        <InnerCircle1 />
-        <InnerCircle2 />
-        <InnerCircle3 />
-      </InnerCircle>
-      <Wbutton />
-    </SendBox>
+    <></>
+    //     <SendBox>
+    //       <InnerCircle>
+    //         <InnerCircle1 />
+    //         <InnerCircle2 />
+    //         <InnerCircle3 />
+    //       </InnerCircle>
+    //     </SendBox>
   )
 }
