40b471db4fc8597b5fd440d9284c79796d29b228
조은정 <ejjeongs@naver.com>
2023-11-06 17:41:33 +0900

    Refactor: delete console
    Change-Id: Ice26bce1fd028f52149f00521d99948d621ccb4e

--- front/frontend/src/app/utils/write/createNote.ts
+++ front/frontend/src/app/utils/write/createNote.ts
@@ -30,7 +30,6 @@ export const createNotereset = async (): Promise<AxiosResponse> => {
     if (!res || res.status !== 200) {
       throw new Error('에러')
     }
-    console.log(res)
     return res.data
   } catch (error) {
     throw new Error('네트워크 오류')
