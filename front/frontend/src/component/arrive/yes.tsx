5688e785d1b34b5156a621dfe9891401da288f1d
권은정 <un2642@naver.com>
2023-11-07 16:20:56 +0900

    Feat: 롤링페이퍼 링크 생성로직
    Change-Id: I76e87adcd33135f256f996978c9dba07753e5759

--- front/frontend/src/component/arrive/yes.tsx
+++ front/frontend/src/component/arrive/yes.tsx
@@ -1,32 +1,38 @@
 import { useRouter } from 'next/navigation'
 import DeliveryUfo from '../Three/deliveryUfo'
+import { MakeLinkAxios } from '@/app/utils/main/makeLinkAxios'
+import { useEffect, useState } from 'react'
+
 import * as a from './arrive.styled'
 
 export default function Yes() {
   const router = useRouter()
+  const [rollId, setRollId] = useState()
+
   const handleClick = (url: string) => {
     router.push(url)
   }
+
+  useEffect(() => {
+    async function click() {
+      const check = await MakeLinkAxios()
+      console.log('요기')
+      console.log(check.data)
+      setRollId(check.data.rollId)
+    }
+    click()
+  }, [])
+
   return (
     <div>
-      <div
-        style={{ marginTop: '650px' }}
-        className="mt-20 flex items-center justify-center pt-20 text-2xl text-white"
-      >
+      <div className="flex items-center justify-center pb-10 text-2xl text-white">
         편지가 도착했어요~
       </div>
-      <div
-        style={{
-          width: '100%',
-          height: '200px',
-          marginTop: '50px',
-          marginBottom: '50px',
-        }}
-      >
+      <a.UfoStyle>
         <DeliveryUfo style={{ width: '100%', height: '100%' }} />
-      </div>
+      </a.UfoStyle>
       <div
-        style={{ flexDirection: 'column' }}
+        style={{ flexDirection: 'row', gap: '1rem' }}
         className="flex items-center justify-center text-2xl"
       >
         <a.arriveButton onClick={() => handleClick('/today/storage/friend')}>
@@ -35,7 +41,9 @@ export default function Yes() {
         <a.arriveButton onClick={() => handleClick('/today/storage/Random')}>
           <a.arriveLink>랜덤</a.arriveLink>
         </a.arriveButton>
-        <a.arriveButton onClick={() => handleClick('/today/storage/Link')}>
+        <a.arriveButton
+          onClick={() => handleClick(`/today/storage/Link/${rollId}`)}
+        >
           <a.arriveLink>링크</a.arriveLink>
         </a.arriveButton>
       </div>
