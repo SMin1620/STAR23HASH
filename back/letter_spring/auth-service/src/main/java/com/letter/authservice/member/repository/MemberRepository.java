1015614653cdfb3fb48c33953601a10355fadf9b
이승민 <dltmdals1620@gmail.com>
2023-11-07 11:13:44 +0900

    Feat : 인증 서버 - 알람 처리
    Change-Id: Ied3ae582f4f19020b5d786e7bfb1e06e3dfcc345

--- back/letter_spring/auth-service/src/main/java/com/letter/authservice/member/repository/MemberRepository.java
+++ back/letter_spring/auth-service/src/main/java/com/letter/authservice/member/repository/MemberRepository.java
@@ -4,6 +4,7 @@ import com.letter.authservice.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,4 +24,9 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
      * 작성 제한 스케줄러
      */
     List<Member> findAllByIsWriteTrue();
+
+    /**
+     * 알람 체크
+     */
+    Optional<Member> findByIdAndCreateAtBetween(Long memberId, LocalDateTime start, LocalDateTime end);
 }
