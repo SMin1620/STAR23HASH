3638e55c6b264868234f89176841b21c2d048a53
이승민 <dltmdals1620@gmail.com>
2023-11-07 11:14:57 +0900

    Feat : 인증 서버 - 롤링페이퍼 알람 비즈니스 로직
    Change-Id: Ib8185c062b72fd553323ef06b9072f7bec20b755

--- back/letter_spring/auth-service/src/main/java/com/letter/authservice/member/repository/RollRepository.java
+++ back/letter_spring/auth-service/src/main/java/com/letter/authservice/member/repository/RollRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface RollRepository extends JpaRepository<Roll, Long> {
-    Optional<Roll> findRollByMemberId(Long memberId);
+    Optional<Roll> findByMemberId(Long memberId);
 }
