69b5f255ad769d27d8e47024616d7706d2322035
권은정 <un2642@naver.com>
2023-11-07 16:20:56 +0900

    Feat: 롤링페이퍼 링크 생성로직
    Change-Id: I76e87adcd33135f256f996978c9dba07753e5759

---
+++ front/frontend/src/app/today/arrive/arrive.styled.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const ContentBox = styled.div`
+  position: absolute;
+  transform: translate(-50%, -50%);
+  top: 50%;
+  left: 50%;
+  z-index: 20;
+`
