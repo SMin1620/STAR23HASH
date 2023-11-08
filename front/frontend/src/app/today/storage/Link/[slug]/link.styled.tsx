cb11a4a51ca5a40384418dd0d9747a88b360933e
김예슬 <pdy051257@naver.com>
2023-11-08 15:36:53 +0900

    Design: 오늘의 링크 보관함 - 디자인 업데이트
    Change-Id: I34d7a494a76f15a09b4c4090181ba8611824147b

---
+++ front/frontend/src/app/today/storage/Link/[slug]/link.styled.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+export const LinkContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`
+export const LinkButton = styled.button`
+  background-color: #3e3d83;
+  position: absolute;
+  width: 100px;
+  height: 50px;
+  bottom: 70px;
+  left: 0;
+  right: 0;
+  margin: auto;
+  border-radius: 20px;
+  color: white;
+  font-size: 21px;
+  box-shadow: 3px 3px #545454;
+  pointer-events: auto;
+`
+export const HtmlContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 3;
+  pointer-events: none;
+`
