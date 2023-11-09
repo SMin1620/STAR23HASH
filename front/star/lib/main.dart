eb0f9ad07b2cd85b43179ca1620dbadd9d1b8c6c
조은정 <ejjeongs@naver.com>
2023-11-09 11:49:10 +0900

    Feat: Add feature back
    Change-Id: Ie890ee176c5e79a074c1ad57634ae88a26ef2f27

--- front/star/lib/main.dart
+++ front/star/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:dio/dio.dart';
+
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
@@ -35,22 +36,30 @@ class InAppWebViewPage extends StatefulWidget {
   _InAppWebViewPageState createState() => new _InAppWebViewPageState();
 }
 
-
 class _InAppWebViewPageState extends State<InAppWebViewPage> {
   late InAppWebViewController _webViewController;
 
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: Container(
-                child: InAppWebView(
+    return WillPopScope(
+      onWillPop: () async {
+        if (await _webViewController.canGoBack()) {
+          _webViewController.goBack();
+          return Future.value(false);
+        } else {
+          return Future.value(true);
+        }
+      },
+      child: Scaffold(
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  child: InAppWebView(
                     initialUrlRequest: URLRequest(
-                      url: Uri.parse("http://k9e106.p.ssafy.io:3000/auth/loginMain"),
+                      url: Uri.parse(
+                          "http://k9e106.p.ssafy.io:3000/auth/loginMain"),
                     ),
                     initialOptions: InAppWebViewGroupOptions(
                       crossPlatform: InAppWebViewOptions(
@@ -62,64 +71,84 @@ class _InAppWebViewPageState extends State<InAppWebViewPage> {
                       _webViewController = controller;
                     },
                     androidOnPermissionRequest:
-                        (InAppWebViewController controller, String origin, List<String> resources) async {
+                        (InAppWebViewController controller, String origin,
+                            List<String> resources) async {
                       return PermissionRequestResponse(
                         resources: resources,
                         action: PermissionRequestResponseAction.GRANT,
                       );
                     },
-                    onProgressChanged: (InAppWebViewController controller, int progress) async {
+                    onProgressChanged: (InAppWebViewController controller,
+                        int progress) async {
                       Uri? currentUrl = await _webViewController.getUrl();
                       print(currentUrl);
-                      if (currentUrl.toString() == "http://k9e106.p.ssafy.io:3000/pullfriend") {
+                      print(progress);
+                      if (currentUrl.toString() ==
+                          "http://k9e106.p.ssafy.io:3000/pullfriend") {
                         List<Contact> contacts = [];
-                        PermissionStatus status = await Permission.contacts.request();
+                        PermissionStatus status =
+                            await Permission.contacts.request();
                         if (status.isGranted) {
                           try {
-                            Iterable<Contact> contactList = await ContactsService.getContacts();
+                            Iterable<Contact> contactList =
+                                await ContactsService.getContacts();
                             setState(() {
                               contacts = contactList.toList();
-                              print("contact");
-
-                              sendContactsToServer(contacts);
                             });
+                              if(await sendContactsToServer(contacts)) {
+                                controller.loadUrl(
+                                  urlRequest: URLRequest(
+                                    url: Uri.parse(
+                                        "http://k9e106.p.ssafy.io:3000/write/wfriend/getfriend"),
+                                  ),
+                                );
+                              }
                           } on PlatformException catch (e) {
                             print('Error: ${e.message}');
                           }
                         }
-                        controller.loadUrl(
-                          urlRequest: URLRequest(
-                            url: Uri.parse("http://k9e106.p.ssafy.io:3000/write/wfriend/getfriend"),
-                          ),
-                        );
+                        else {
+                          controller.loadUrl(
+                            urlRequest: URLRequest(
+                              url: Uri.parse(
+                                  "http://k9e106.p.ssafy.io:3000/write"),
+                            ),
+                          );
+                        }
+
                       }
                     },
-
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-void sendContactsToServer(List<Contact> contacts) async {
-  List<Map<String, String>> contactList = contacts.map((contact) {
+Future<bool> sendContactsToServer(List<Contact> contacts) async {
+  List<Map<String, String>> contactList = contacts.where((contact) => contact.phones?.isNotEmpty ?? false).map((contact) {
     String name = contact.displayName ?? '';
-    String phoneNumber = contact.phones?.first.value ?? '';
+    String phoneNumber = contact.phones!.first.value!;
     phoneNumber = phoneNumber.replaceAll(RegExp(r'[^0-9]'), '');
     return {
       'name': name,
       'phone': phoneNumber,
     };
   }).toList();
+
   print(contactList.toString());
   var cookieManager = CookieManager.instance();
-  var accessToken = await cookieManager.getCookie(url: Uri.parse("http://k9e106.p.ssafy.io:3000/write"), name: "accessToken");
+  var accessToken = await cookieManager.getCookie(
+      url: Uri.parse("http://k9e106.p.ssafy.io:3000/write"),
+      name: "accessToken");
+
   if (accessToken != null) {
     var accessTokenValue = accessToken.value;
+    print(accessTokenValue);
     var dio = Dio();
     dio.options.headers['Authorization'] = '$accessTokenValue';
     dio.options.headers['Cookie'] = accessToken.toString();
@@ -130,14 +159,17 @@ void sendContactsToServer(List<Contact> contacts) async {
       );
       if (response.statusCode == 200) {
         print('Contacts sent to server');
+        return true;
       } else {
         print('Failed to send contacts to server');
+        return false;
       }
     } catch (e) {
       print('Error: $e');
+      return false;
     }
-    // 이후 원하는 작업을 수행할 수 있습니다.
   } else {
     print('accessToken이 없습니다.');
+    return false;
   }
 }
