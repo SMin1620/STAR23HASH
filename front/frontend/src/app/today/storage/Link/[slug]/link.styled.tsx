e40c25c1f65fd39d5fc00ccf09e061fa074ff6bb
김예슬 <pdy051257@naver.com>
2023-11-09 11:21:04 +0900

    Design : 버튼 디자인 수정
    Change-Id: I2fe3ba47f4e0414ae7f3adff1bb7c426351d086d

--- front/frontend/src/app/today/storage/Link/[slug]/link.styled.tsx
+++ front/frontend/src/app/today/storage/Link/[slug]/link.styled.tsx
@@ -10,14 +10,15 @@ export const LinkButton = styled.button`
   position: absolute;
   width: 100px;
   height: 50px;
-  bottom: 70px;
+  bottom: 120px;
   left: 0;
   right: 0;
   margin: auto;
   border-radius: 20px;
   color: white;
   font-size: 21px;
-  box-shadow: 3px 3px #545454;
+  box-shadow: 0px 6px 5px -2px #545454;
+
   pointer-events: auto;
 `
 export const HtmlContainer = styled.div`
@@ -32,9 +33,10 @@ export const HtmlContainer = styled.div`
 
 export const BackButton = styled.div`
   position: absolute;
-  bottom: 15px;
-  left: 17px;
+  bottom: 20px;
+  left: 20px;
   color: white;
   pointer-events: auto;
-  font-size: 18px;
+  font-size: 22px;
+  font-weight: 500;
 `
