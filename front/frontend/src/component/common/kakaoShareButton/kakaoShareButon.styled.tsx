78b6348942ffc35c34288bd25683658be53706dd
김예슬 <pdy051257@naver.com>
2023-11-10 11:24:36 +0900

    Design : 카카오 공유하기 버튼 디자인
    Change-Id: I9c640a2aa316248174e100720945f19f3c8d94a0

---
+++ front/frontend/src/component/common/kakaoShareButton/kakaoShareButon.styled.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const KakaoContainer = styled.div`
+  pointer-events: auto;
+  cursor: pointer;
+  position: absolute;
+  bottom: 110px;
+  left: 50px;
+`
