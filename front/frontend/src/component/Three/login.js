926443bfd79e77a1cd108714ca53fa74cfa88e93
권은정 <un2642@naver.com>
2023-11-10 11:02:42 +0900

    Fix: Migrate main page
    Change-Id: Ic574dad0480cc211df2765b16ca71892d0bd2eea

--- front/frontend/src/component/Three/login.js
+++ front/frontend/src/component/Three/login.js
@@ -54,7 +54,7 @@ function Test() {
   useFrame(({ clock }) => {
     // y축으로 조금씩 이동
     ref.current.position.y += Math.sin(clock.getElapsedTime()) * 0.001
-  })
+  }, [])
 
   return <primitive object={gltf.scene} ref={ref} />
 }
@@ -97,7 +97,7 @@ function Asteroid() {
       ref.current.position.y = startY
       ref.current.position.z = startZ
     }
-  })
+  }, [])
 
   return <primitive object={gltf.scene} ref={ref} />
 }
