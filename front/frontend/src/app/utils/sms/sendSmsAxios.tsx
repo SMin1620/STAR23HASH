1daf953357ab3ef13f65c13732fa3ae605ac74d7
권은정 <un2642@naver.com>
2023-11-11 15:48:26 +0900

    Feat: 전화번호 인증sms
    Change-Id: Ia7abfe193224361af3a46753bc095b69baa64fd5

---
+++ front/frontend/src/app/utils/sms/sendSmsAxios.tsx
@@ -0,0 +1,28 @@
+import axios, { AxiosResponse, AxiosError } from 'axios'
+import { log } from 'console'
+
+const DOMAIN = process.env.NEXT_PUBLIC_API_URL
+
+const axiosInstance = axios.create({
+  withCredentials: true,
+})
+
+export const sendSmsAxios = async (
+  phone: string,
+): Promise<AxiosResponse> => {
+  try {
+    const res: AxiosResponse = await axiosInstance.post(
+      `${DOMAIN}/api/sms/sends`,
+      {
+        phone: phone,
+      },
+    )
+    if (!res || res.status !== 200) {
+      throw new Error('에러')
+    }
+
+    return res
+  } catch (error) {
+    throw new Error('네트워크 오류')
+  }
+}
