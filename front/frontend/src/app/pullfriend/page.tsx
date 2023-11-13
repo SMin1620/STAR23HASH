ed7ef44454ecd86b6c57f8b4b6605017f58d2dbe
조은정 <ejjeongs@naver.com>
2023-11-13 11:33:32 +0900

    Feat: Add searching_member modal
    Change-Id: I7e85d4c971a59f386b6d6bd8f0b4db4cda061c4d

--- front/frontend/src/app/pullfriend/page.tsx
+++ front/frontend/src/app/pullfriend/page.tsx
@@ -1,39 +1,34 @@
 'use client'
+import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import GlobalStyle from '../write/GlobalStyles'
 import * as st from './pullfriend.styled'
 import * as stt from '@/component/common/write_layout/write_layout.styled'
-import { pullContacts } from '@/app/utils/write/pullContacts'
 import { useRouter, useSearchParams } from 'next/navigation'
+import SearchingModal from '@/component/write/searchingmodal'
 type Contact = {
   name: string
   phone: string
 }
 
 export default function PullFriend() {
+  const [showModal, setShowModal] = useState(false)
+  const [contact, setContact] = useState('')
   const router = useRouter()
 
-  // const contacts: Contact[] = [
-  //   { name: '김은정0', phone: '1234' },
-  //   { name: '이은정1', phone: '12345' },
-  //   { name: '박은정2', phone: '99' },
-  //   { name: '권은정3', phone: '999' },
-  //   { name: '조은정4', phone: '9999' },
-  //   { name: '권은정5', phone: '01012341234' },
-  //   { name: '조은정6', phone: '01012341234' },
-  //   { name: '조은정7', phone: '01012341234' },
-  //   { name: '조은정8', phone: '01012341234' },
-  //   { name: '조은정9', phone: '01012341234' },
-  //   { name: '조은정10', phone: '01012341234' },
-  //   { name: '조은정11', phone: '01012341234' },
-  //   { name: '조은정12', phone: '01012341234' },
-  //   { name: '조은정13', phone: '01012341234' },
-  //   { name: '조은정14', phone: '01012341234' },
-  // ]
-  // const handleSendClick = async () => {
-  //   await pullContacts(contacts)
-  //   router.replace(`/write/wfriend/getfriend`)
-  // }
+  function openModal() {
+    setShowModal(true)
+  }
+
+  const handleInputText = (contact: string) => {
+    setContact(contact)
+    setShowModal(false)
+    // 상위 컴포넌트에서 inputText를 처리하는 로직을 추가해주세요.
+    if (contact !== '') {
+      router.replace(`/write/wfriend/input?phone=${contact}`)
+    }
+  }
+
   return (
     <>
       <GlobalStyle />
@@ -48,11 +43,14 @@ export default function PullFriend() {
             <st.WhoSend>연락처를 가져와야 해요! </st.WhoSend>
             <st.WhoSend>연락처 접근 권한을 허용해주세요 </st.WhoSend>
           </st.ContentBox>
-          {/* <stt.EmptyDiv>
-            <stt.button onClick={handleSendClick}>전송</stt.button>
-          </stt.EmptyDiv> */}
+          <stt.EmptyDiv>
+            <stt.button onClick={openModal}>조회하기</stt.button>
+          </stt.EmptyDiv>
         </stt.SendBox>
       </stt.SendBoxDiv>
+      {showModal && (
+        <SearchingModal contact={contact} closeState={handleInputText} />
+      )}
     </>
   )
 }
