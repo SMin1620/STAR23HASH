aca32a4850bd62691cea2088d86ff79e6e6db0e3
권은정 <un2642@naver.com>
2023-11-14 16:48:08 +0900

    Design: 인증번호 ui 변경
    Change-Id: Ifdf12e231fb326dad2d0e5f9c3ceea0b41faada9

--- front/frontend/src/app/auth/regist/page.tsx
+++ front/frontend/src/app/auth/regist/page.tsx
@@ -8,8 +8,11 @@ import * as r from './regist.styled'
 
 export default function Regist() {
   const router = useRouter()
-  const { phone } = PhoneStore()
+  // const { phone } = PhoneStore()
+  const [phone, setPhone] = useState('01048436836')
   const [inputValue, setInputValue] = useState('')
+  const [isAuthSent, setAuthSent] = useState(false) // 인증번호 전송 성공 여부 상태
+  const [isAuthReceive, setAuthReceive] = useState(false) // 인증번호 전송 성공 여부 상태
 
   // 인증확인 로직
   async function PasswordAuthClick() {
@@ -38,6 +41,7 @@ export default function Regist() {
         const check = await sendSmsAxios(phone)
         if (check.data.data) {
           alert('인증번호가 전송되었습니다.')
+          setAuthSent(true) // 인증번호 전송 성공
         } else {
           throw new Error()
         }
@@ -55,28 +59,37 @@ export default function Regist() {
     <div className="flex flex-col items-center justify-center">
       <div className="mb-2 text-5xl text-white">{phone}</div>
       <r.RegistButton onClick={changePhone}>전화번호 다시쓰기</r.RegistButton>
-      <r.RegistButton onClick={sendAuthClick}>인증번호 전송</r.RegistButton>
-      <input
-        style={{ width: '220px', height: '35px' }}
-        className="mb-2 rounded-lg text-center text-sm opacity-60	"
-        placeholder="인증번호를 입력해주세요"
-        name="phone"
-        type="string"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-      ></input>
-
-      <button
-        onClick={PasswordAuthClick}
-        style={{
-          width: '100px',
-          backgroundColor: '#D9D9D9',
-          padding: '10px 20px 10px 20px',
-          borderRadius: '10px',
-        }}
+      <r.RegistButtonR
+        onClick={sendAuthClick}
+        style={{ backgroundColor: isAuthSent ? '#d9d9d9' : '' }} // 인증번호 전송 성공 시 배경색 변경
       >
-        인증
-      </button>
+        인증번호 전송
+      </r.RegistButtonR>
+      {isAuthSent && ( // 인증번호 전송 성공 시만 렌더링
+        <>
+          <input
+            style={{ width: '220px', height: '35px' }}
+            className="mb-2 rounded-lg text-center text-sm opacity-60	"
+            placeholder="인증번호를 입력해주세요"
+            name="phone"
+            type="string"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          ></input>
+
+          <button
+            onClick={PasswordAuthClick}
+            style={{
+              width: '100px',
+              backgroundColor: '#FFE189',
+              padding: '10px 20px 10px 20px',
+              borderRadius: '10px',
+            }}
+          >
+            인증
+          </button>
+        </>
+      )}
     </div>
   )
 }
