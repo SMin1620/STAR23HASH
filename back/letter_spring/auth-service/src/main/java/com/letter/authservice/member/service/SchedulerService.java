ae0d686b635c58ce4e81340b2b4c7c88e2fefb2b
이승민 <dltmdals1620@gmail.com>
2023-11-17 12:24:49 +0900

    Fix : 인증 서버 - 배포용 시간 변경
    Change-Id: I63dd6ab53c456b2cc0d0908e41333c8b5e5e3931

--- back/letter_spring/auth-service/src/main/java/com/letter/authservice/member/service/SchedulerService.java
+++ back/letter_spring/auth-service/src/main/java/com/letter/authservice/member/service/SchedulerService.java
@@ -16,7 +16,7 @@ public class SchedulerService {
     private final MemberRepository memberRepository;
 
     @Transactional
-    @Scheduled(cron = "0 0 10 * * ?")
+    @Scheduled(cron = "0 0 17 * * ?")
     public void reset() {
         List<Member> memberList = memberRepository.findAllByIsWriteTrue();
         for (Member member : memberList) member.setIsWrite(false);
